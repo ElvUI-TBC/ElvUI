2461ebff68cd60b1ae325a88ffd61a5f25a4d971
Bunny <zolov6767@gmail.com>
2017-03-19 00:00:24 +0300

    realise minimap module

--- ElvUI/modules/maps/minimap.lua
+++ ElvUI/modules/maps/minimap.lua
@@ -1,116 +1,112 @@
-local E, L, V, P, G = unpack(ElvUI);
-local M = E:NewModule("Minimap", "AceEvent-3.0");
-E.Minimap = M;
+local E, L, V, P, G = unpack(ElvUI)
+local M = E:NewModule("Minimap", "AceEvent-3.0")
+E.Minimap = M
 
-local _G = _G;
-local strsub = strsub;
+local _G = _G
+local strsub = strsub
 
-local CreateFrame = CreateFrame;
-local ToggleCharacter = ToggleCharacter;
-local ToggleFrame = ToggleFrame;
-local ToggleAchievementFrame = ToggleAchievementFrame;
-local ToggleFriendsFrame = ToggleFriendsFrame;
-local IsAddOnLoaded = IsAddOnLoaded;
-local ToggleHelpFrame = ToggleHelpFrame;
-local GetZonePVPInfo = GetZonePVPInfo;
-local IsShiftKeyDown = IsShiftKeyDown;
-local ToggleDropDownMenu = ToggleDropDownMenu;
-local Minimap_OnClick = Minimap_OnClick;
-local GetMinimapZoneText = GetMinimapZoneText;
-local InCombatLockdown = InCombatLockdown;
+local CreateFrame = CreateFrame
+local ToggleCharacter = ToggleCharacter
+local ToggleFrame = ToggleFrame
+local ToggleAchievementFrame = ToggleAchievementFrame
+local ToggleFriendsFrame = ToggleFriendsFrame
+local IsAddOnLoaded = IsAddOnLoaded
+local ToggleHelpFrame = ToggleHelpFrame
+local GetZonePVPInfo = GetZonePVPInfo
+local IsShiftKeyDown = IsShiftKeyDown
+local ToggleDropDownMenu = ToggleDropDownMenu
+local Minimap_OnClick = Minimap_OnClick
+local GetMinimapZoneText = GetMinimapZoneText
+local InCombatLockdown = InCombatLockdown
 
-local menuFrame = CreateFrame("Frame", "MinimapRightClickMenu", E.UIParent, "UIDropDownMenuTemplate");
+local menuFrame = CreateFrame("Frame", "MinimapRightClickMenu", E.UIParent, "UIDropDownMenuTemplate")
 local menuList = {
 	{text = CHARACTER_BUTTON,
-	func = function() ToggleCharacter("PaperDollFrame"); end},
+	func = function() ToggleCharacter("PaperDollFrame") end},
 	{text = SPELLBOOK_ABILITIES_BUTTON,
-	func = function() ToggleFrame(SpellBookFrame); end},
+	func = function() ToggleFrame(SpellBookFrame) end},
 	{text = TALENTS_BUTTON,
-	func = function() ToggleTalentFrame(); end},
+	func = function() ToggleTalentFrame() end},
 	{text = ACHIEVEMENT_BUTTON,
-	func = function() ToggleAchievementFrame(); end},
+	func = function() ToggleAchievementFrame() end},
 	{text = QUESTLOG_BUTTON,
-	func = function() ToggleFrame(QuestLogFrame); end},
+	func = function() ToggleFrame(QuestLogFrame) end},
 	{text = SOCIAL_BUTTON,
-	func = function() ToggleFriendsFrame(1); end},
+	func = function() ToggleFriendsFrame(1) end},
 	{text = L["Calendar"],
-	func = function() GameTimeFrame:Click(); end},
+	func = function() GameTimeFrame:Click() end},
 	{text = L["Farm Mode"],
 	func = FarmMode},
 	{text = TIMEMANAGER_TITLE,
-	func = function() ToggleTimeManager(); end},
+	func = function() ToggleTimeManager() end},
 	{text = PLAYER_V_PLAYER,
-	func = function() ToggleFrame(PVPParentFrame); end},
+	func = function() ToggleFrame(PVPParentFrame) end},
 	{text = LFG_TITLE,
-	func = function() ToggleFrame(LFDParentFrame); end},
+	func = function() ToggleFrame(LFDParentFrame) end},
 	{text = L_LFRAID,
-	func = function() ToggleFrame(LFRParentFrame); end},
+	func = function() ToggleFrame(LFRParentFrame) end},
 	{text = HELP_BUTTON,
-	func = function() ToggleHelpFrame(); end},
+	func = function() ToggleHelpFrame() end},
 	{text = L_CALENDAR,
 	func = function()
 		if(not CalendarFrame) then
-			LoadAddOn("Blizzard_Calendar");
+			LoadAddOn("Blizzard_Calendar")
 		end
-		Calendar_Toggle();
+		Calendar_Toggle()
 	end}
-};
-
-function GetMinimapShape()
-	return "SQUARE";
-end
+}
 
 function M:GetLocTextColor()
-	local pvpType = GetZonePVPInfo();
-	if(pvpType == "sanctuary") then
-		return 0.035, 0.58, 0.84;
-	elseif(pvpType == "arena") then
-		return 0.84, 0.03, 0.03;
-	elseif(pvpType == "friendly") then
-		return 0.05, 0.85, 0.03;
-	elseif(pvpType == "hostile") then
-		return 0.84, 0.03, 0.03;
-	elseif(pvpType == "contested") then
-		return 0.9, 0.85, 0.05;
+	local pvpType = GetZonePVPInfo()
+	if pvpType == "sanctuary" then
+		return 0.035, 0.58, 0.84
+	elseif pvpType == "arena" then
+		return 0.84, 0.03, 0.03
+	elseif pvpType == "friendly" then
+		return 0.05, 0.85, 0.03
+	elseif pvpType == "hostile" then
+		return 0.84, 0.03, 0.03
+	elseif pvpType == "contested" then
+		return 0.9, 0.85, 0.05
 	else
-		return 0.84, 0.03, 0.03;
+		return 0.84, 0.03, 0.03
 	end
 end
 
 function M:ADDON_LOADED(_, addon)
-	if(addon == "Blizzard_TimeManager") then
-		TimeManagerClockButton:Kill();
+	if addon == "Blizzard_TimeManager" then
+		TimeManagerClockButton:Kill()
 	end
 end
 
 function M:Minimap_OnMouseUp(btn)
-	local position = self:GetPoint();
-	if(btn == "MiddleButton" or (btn == "RightButton" and IsShiftKeyDown())) then
-		if(position:match("LEFT")) then
-			EasyMenu(menuList, menuFrame, "cursor", 0, 0, "MENU", 2);
+	local position = self:GetPoint()
+	if btn == "MiddleButton" or (btn == "RightButton" and IsShiftKeyDown()) then
+		if position:match("LEFT") then
+			EasyMenu(menuList, menuFrame, "cursor", 0, 0, "MENU", 2)
 		else
-			EasyMenu(menuList, menuFrame, "cursor", -160, 0, "MENU", 2);
+			EasyMenu(menuList, menuFrame, "cursor", -160, 0, "MENU", 2)
 		end
-	elseif(btn == "RightButton") then
-		ToggleDropDownMenu(1, nil, MiniMapTrackingDropDown, "cursor");
+	elseif btn == "RightButton" then
+		ToggleDropDownMenu(1, nil, MiniMapTrackingDropDown, "cursor")
 	else
-		Minimap_OnClick(self);
+		Minimap_OnClick(self)
 	end
 end
 
 function M:Minimap_OnMouseWheel(d)
-	if(d > 0) then
-		_G.MinimapZoomIn:Click();
-	elseif(d < 0) then
-		_G.MinimapZoomOut:Click();
+	if d > 0 then
+		_G.MinimapZoomIn:Click()
+	elseif d < 0 then
+		_G.MinimapZoomOut:Click()
 	end
 end
 
 function M:Update_ZoneText()
-	if E.db.general.minimap.locationText == "HIDE" or not E.private.general.minimap.enable then return; end
-	Minimap.location:SetText(strsub(GetMinimapZoneText(),1,46))
+	if E.db.general.minimap.locationText == "HIDE" or not E.private.general.minimap.enable then return end
+	Minimap.location:SetText(strsub(GetMinimapZoneText(), 1, 46))
 	Minimap.location:SetTextColor(self:GetLocTextColor())
-	Minimap.location:FontTemplate(E.LSM:Fetch("font", E.db.general.minimap.locationFont), E.db.general.minimap.locationFontSize, E.db.general.minimap.locationFontOutline);
+	Minimap.location:FontTemplate(E.LSM:Fetch("font", E.db.general.minimap.locationFont), E.db.general.minimap.locationFontSize, E.db.general.minimap.locationFontOutline)
 end
 
 function M:PLAYER_REGEN_ENABLED()
@@ -118,120 +114,114 @@ function M:PLAYER_REGEN_ENABLED()
 	self:UpdateSettings()
 end
 
-local isResetting;
+local isResetting
 local function ResetZoom()
-	Minimap:SetZoom(0);
-	MinimapZoomIn:Enable();
-	MinimapZoomOut:Disable();
-	isResetting = false;
+	Minimap:SetZoom(0)
+	MinimapZoomIn:Enable()
+	MinimapZoomOut:Disable()
+	isResetting = false
 end
 local function SetupZoomReset()
-	if(E.db.general.minimap.resetZoom.enable and not isResetting) then
-		isResetting = true;
-		E:Delay(E.db.general.minimap.resetZoom.time, ResetZoom);
+	if E.db.general.minimap.resetZoom.enable and not isResetting then
+		isResetting = true
+		E:Delay(E.db.general.minimap.resetZoom.time, ResetZoom)
 	end
 end
-hooksecurefunc(Minimap, "SetZoom", SetupZoomReset);
+hooksecurefunc(Minimap, "SetZoom", SetupZoomReset)
 
 function M:UpdateSettings()
 	if InCombatLockdown() then
 		self:RegisterEvent("PLAYER_REGEN_ENABLED")
 	end
 
-	E.MinimapSize = E.private.general.minimap.enable and E.db.general.minimap.size or Minimap:GetWidth() + 10;
-	E.MinimapWidth = E.MinimapSize;
-	E.MinimapHeight = E.MinimapSize;
+	E.MinimapSize = E.private.general.minimap.enable and E.db.general.minimap.size or Minimap:GetWidth() + 10
+	E.MinimapWidth = E.MinimapSize
+	E.MinimapHeight = E.MinimapSize
 
-	if(E.db.general.reminder.enable) then
-		E.RBRWidth = (E.MinimapHeight + ((E.Border - E.Spacing*3) * 5) + E.Border*2) / 6;
-	else
-		E.RBRWidth = 0;
+	if E.private.general.minimap.enable then
+		Minimap:Size(E.MinimapSize, E.MinimapSize)
 	end
 
-	if(E.private.general.minimap.enable) then
-		Minimap:Size(E.MinimapSize, E.MinimapSize);
-	end
-
-	if(LeftMiniPanel and RightMiniPanel) then
-		if(E.db.datatexts.minimapPanels and E.private.general.minimap.enable) then
-			LeftMiniPanel:Show();
-			RightMiniPanel:Show();
+	if LeftMiniPanel and RightMiniPanel then
+		if E.db.datatexts.minimapPanels and E.private.general.minimap.enable then
+			LeftMiniPanel:Show()
+			RightMiniPanel:Show()
 		else
-			LeftMiniPanel:Hide();
-			RightMiniPanel:Hide();
+			LeftMiniPanel:Hide()
+			RightMiniPanel:Hide()
 		end
 	end
 
-	if(BottomMiniPanel) then
-		if(E.db.datatexts.minimapBottom and E.private.general.minimap.enable) then
-			BottomMiniPanel:Show();
+	if BottomMiniPanel then
+		if E.db.datatexts.minimapBottom and E.private.general.minimap.enable then
+			BottomMiniPanel:Show()
 		else
-			BottomMiniPanel:Hide();
+			BottomMiniPanel:Hide()
 		end
 	end
 
-	if(BottomLeftMiniPanel) then
-		if(E.db.datatexts.minimapBottomLeft and E.private.general.minimap.enable) then
-			BottomLeftMiniPanel:Show();
+	if BottomLeftMiniPanel then
+		if E.db.datatexts.minimapBottomLeft and E.private.general.minimap.enable then
+			BottomLeftMiniPanel:Show()
 		else
-			BottomLeftMiniPanel:Hide();
+			BottomLeftMiniPanel:Hide()
 		end
 	end
 
-	if(BottomRightMiniPanel) then
-		if(E.db.datatexts.minimapBottomRight and E.private.general.minimap.enable) then
-			BottomRightMiniPanel:Show();
+	if BottomRightMiniPanel then
+		if E.db.datatexts.minimapBottomRight and E.private.general.minimap.enable then
+			BottomRightMiniPanel:Show()
 		else
-			BottomRightMiniPanel:Hide();
+			BottomRightMiniPanel:Hide()
 		end
 	end
 
-	if(TopMiniPanel) then
-		if(E.db.datatexts.minimapTop and E.private.general.minimap.enable) then
-			TopMiniPanel:Show();
+	if TopMiniPanel then
+		if E.db.datatexts.minimapTop and E.private.general.minimap.enable then
+			TopMiniPanel:Show()
 		else
-			TopMiniPanel:Hide();
+			TopMiniPanel:Hide()
 		end
 	end
 
-	if(TopLeftMiniPanel) then
-		if(E.db.datatexts.minimapTopLeft and E.private.general.minimap.enable) then
-			TopLeftMiniPanel:Show();
+	if TopLeftMiniPanel then
+		if E.db.datatexts.minimapTopLeft and E.private.general.minimap.enable then
+			TopLeftMiniPanel:Show()
 		else
-			TopLeftMiniPanel:Hide();
+			TopLeftMiniPanel:Hide()
 		end
 	end
 
-	if(TopRightMiniPanel) then
-		if(E.db.datatexts.minimapTopRight and E.private.general.minimap.enable) then
-			TopRightMiniPanel:Show();
+	if TopRightMiniPanel then
+		if E.db.datatexts.minimapTopRight and E.private.general.minimap.enable then
+			TopRightMiniPanel:Show()
 		else
-			TopRightMiniPanel:Hide();
+			TopRightMiniPanel:Hide()
 		end
 	end
 
-	if(MMHolder) then
-		MMHolder:SetWidth((Minimap:GetWidth() + E.Border + E.Spacing*3) + E.RBRWidth);
+	if MMHolder then
+		MMHolder:Width((Minimap:GetWidth() + E.Border + E.Spacing*3))
 
-		if(E.db.datatexts.minimapPanels) then
-			MMHolder:Height(Minimap:GetHeight() + (LeftMiniPanel and (LeftMiniPanel:GetHeight() + E.Border) or 24) + E.Spacing*3);
+		if E.db.datatexts.minimapPanels then
+			MMHolder:Height(Minimap:GetHeight() + (LeftMiniPanel and (LeftMiniPanel:GetHeight() + E.Border) or 24) + E.Spacing*3)
 		else
-			MMHolder:Height(Minimap:GetHeight() + E.Border + E.Spacing*3);
+			MMHolder:Height(Minimap:GetHeight() + E.Border + E.Spacing*3)
 		end
 	end
 
-	if(Minimap.location) then
-		Minimap.location:SetWidth(E.MinimapSize);
+	if Minimap.location then
+		Minimap.location:SetWidth(E.MinimapSize)
 
-		if(E.db.general.minimap.locationText ~= "SHOW" or not E.private.general.minimap.enable) then
-			Minimap.location:Hide();
+		if E.db.general.minimap.locationText ~= "SHOW" or not E.private.general.minimap.enable then
+			Minimap.location:Hide()
 		else
-			Minimap.location:Show();
+			Minimap.location:Show()
 		end
 	end
 
-	if(MinimapMover) then
-		MinimapMover:Size(MMHolder:GetSize());
+	if MinimapMover then
+		MinimapMover:Size(MMHolder:GetSize())
 	end
 
 	if GameTimeFrame then
@@ -284,7 +274,7 @@ function M:UpdateSettings()
 end
 
 function M:Initialize()
-	menuFrame:SetTemplate("Transparent", true);
+	menuFrame:SetTemplate("Transparent", true)
 
 	self:UpdateSettings()
 	if not E.private.general.minimap.enable then
@@ -292,25 +282,29 @@ function M:Initialize()
 		return
 	end
 
+	function GetMinimapShape()
+		return "SQUARE"
+	end
+
 	local mmholder = CreateFrame("Frame", "MMHolder", Minimap)
 	mmholder:Point("TOPRIGHT", E.UIParent, "TOPRIGHT", -3, -3)
-	mmholder:Width((Minimap:GetWidth() + 29))
+	mmholder:Width(Minimap:GetWidth() + 29)
 	mmholder:Height(Minimap:GetHeight() + 53)
-	
+
 	Minimap:ClearAllPoints()
 	Minimap:Point("TOPRIGHT", mmholder, "TOPRIGHT", -E.Border, -E.Border)
 	Minimap:SetMaskTexture("Interface\\ChatFrame\\ChatFrameBackground")
 	Minimap:CreateBackdrop("Default")
 	Minimap:SetFrameLevel(Minimap:GetFrameLevel() + 2)
-	Minimap:HookScript("OnEnter", function(self)
+	Minimap:SetScript("OnEnter", function(self)
 		if E.db.general.minimap.locationText ~= "MOUSEOVER" or not E.private.general.minimap.enable then return end
-		self.location:Show();
-	end);
+		self.location:Show()
+	end)
 
-	Minimap:HookScript("OnLeave", function(self)
+	Minimap:SetScript("OnLeave", function(self)
 		if E.db.general.minimap.locationText ~= "MOUSEOVER" or not E.private.general.minimap.enable then return end
 		self.location:Hide()
-	end);
+	end)
 
 	Minimap.location = Minimap:CreateFontString(nil, "OVERLAY")
 	Minimap.location:FontTemplate(nil, nil, "OUTLINE")
@@ -318,7 +312,7 @@ function M:Initialize()
 	Minimap.location:SetJustifyH("CENTER")
 	Minimap.location:SetJustifyV("MIDDLE")
 	if E.db.general.minimap.locationText ~= "SHOW" or not E.private.general.minimap.enable then
-		Minimap.location:Hide();
+		Minimap.location:Hide()
 	end
 
 	MinimapBorder:Hide()
@@ -342,8 +336,6 @@ function M:Initialize()
 
 	MiniMapWorldMapButton:Hide()
 
-	--MiniMapInstanceDifficulty:SetParent(Minimap)
-
 	if TimeManagerClockButton then
 		TimeManagerClockButton:Kill()
 	end
@@ -365,70 +357,70 @@ function M:Initialize()
 	MinimapBackdrop:ClearAllPoints()
 	MinimapBackdrop:SetAllPoints(Minimap)
 
-	local fm = CreateFrame("Minimap", "FarmModeMap", E.UIParent);
-	fm:Size(E.db.farmSize);
-	fm:Point("TOP", E.UIParent, "TOP", 0, -120);
-	fm:SetClampedToScreen(true);
-	fm:CreateBackdrop("Default");
-	fm:EnableMouseWheel(true);
-	fm:SetScript("OnMouseWheel", M.Minimap_OnMouseWheel);
-	fm:SetScript("OnMouseUp", M.Minimap_OnMouseUp);
-	fm:RegisterForDrag("LeftButton", "RightButton");
-	fm:SetMovable(true);
-	fm:SetScript("OnDragStart", function(self) self:StartMoving(); end);
-	fm:SetScript("OnDragStop", function(self) self:StopMovingOrSizing(); end);
-	fm:Hide();
-	E.FrameLocks["FarmModeMap"] = true;
+	local fm = CreateFrame("Minimap", "FarmModeMap", E.UIParent)
+	fm:Size(E.db.farmSize)
+	fm:Point("TOP", E.UIParent, "TOP", 0, -120)
+	fm:SetClampedToScreen(true)
+	fm:CreateBackdrop("Default")
+	fm:EnableMouseWheel(true)
+	fm:SetScript("OnMouseWheel", M.Minimap_OnMouseWheel)
+	fm:SetScript("OnMouseUp", M.Minimap_OnMouseUp)
+	fm:RegisterForDrag("LeftButton", "RightButton")
+	fm:SetMovable(true)
+	fm:SetScript("OnDragStart", function(self) self:StartMoving() end)
+	fm:SetScript("OnDragStop", function(self) self:StopMovingOrSizing() end)
+	fm:Hide()
+	E.FrameLocks["FarmModeMap"] = true
 
 	FarmModeMap:SetScript("OnShow", function()
 		if(BuffsMover and not E:HasMoverBeenMoved("BuffsMover")) then
-			BuffsMover:ClearAllPoints();
-			BuffsMover:Point("TOPRIGHT", E.UIParent, "TOPRIGHT", -3, -3);
+			BuffsMover:ClearAllPoints()
+			BuffsMover:Point("TOPRIGHT", E.UIParent, "TOPRIGHT", -3, -3)
 		end
 
 		if(DebuffsMover and not E:HasMoverBeenMoved("DebuffsMover")) then
-			DebuffsMover:ClearAllPoints();
-			DebuffsMover:Point("TOPRIGHT", ElvUIPlayerBuffs, "BOTTOMRIGHT", 0, -3);
+			DebuffsMover:ClearAllPoints()
+			DebuffsMover:Point("TOPRIGHT", ElvUIPlayerBuffs, "BOTTOMRIGHT", 0, -3)
 		end
 
-		MinimapCluster:ClearAllPoints();
-		MinimapCluster:SetAllPoints(FarmModeMap);
+		MinimapCluster:ClearAllPoints()
+		MinimapCluster:SetAllPoints(FarmModeMap)
 
 		if(IsAddOnLoaded("Routes")) then
-			LibStub("AceAddon-3.0"):GetAddon("Routes"):ReparentMinimap(FarmModeMap);
+			LibStub("AceAddon-3.0"):GetAddon("Routes"):ReparentMinimap(FarmModeMap)
 		end
 
 		if(IsAddOnLoaded("GatherMate2")) then
-			LibStub("AceAddon-3.0"):GetAddon("GatherMate2"):GetModule("Display"):ReparentMinimapPins(FarmModeMap);
+			LibStub("AceAddon-3.0"):GetAddon("GatherMate2"):GetModule("Display"):ReparentMinimapPins(FarmModeMap)
 		end
-	end);
+	end)
 
 	FarmModeMap:SetScript("OnHide", function()
 		if(BuffsMover and not E:HasMoverBeenMoved("BuffsMover")) then
-			E:ResetMovers(L["Player Buffs"]);
+			E:ResetMovers(L["Player Buffs"])
 		end
 
 		if(DebuffsMover and not E:HasMoverBeenMoved("DebuffsMover")) then
-			E:ResetMovers(L["Player Debuffs"]);
+			E:ResetMovers(L["Player Debuffs"])
 		end
 
-		MinimapCluster:ClearAllPoints();
-		MinimapCluster:SetAllPoints(Minimap);
+		MinimapCluster:ClearAllPoints()
+		MinimapCluster:SetAllPoints(Minimap)
 
 		if(IsAddOnLoaded("Routes")) then
 			LibStub("AceAddon-3.0"):GetAddon("Routes"):ReparentMinimap(Minimap)
 		end
 
 		if(IsAddOnLoaded("GatherMate2")) then
-			LibStub("AceAddon-3.0"):GetAddon("GatherMate2"):GetModule("Display"):ReparentMinimapPins(Minimap);
+			LibStub("AceAddon-3.0"):GetAddon("GatherMate2"):GetModule("Display"):ReparentMinimapPins(Minimap)
 		end
-	end);
+	end)
 
 	UIParent:HookScript("OnShow", function()
 		if(not FarmModeMap.enabled) then
-			FarmModeMap:Hide();
+			FarmModeMap:Hide()
 		end
-	end);
+	end)
 end
 
-E:RegisterModule(M:GetName());
+E:RegisterModule(M:GetName())
