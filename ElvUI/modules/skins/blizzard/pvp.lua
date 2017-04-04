58b106fef7181df01c336e5a7cbbea4af370cd87
Crum <stschleyer89@gmail.com>
2017-04-04 17:26:39 -0500

    PvP Skin (NOT FINISHED)
    Needs more work

--- ElvUI/modules/skins/blizzard/pvp.lua
+++ ElvUI/modules/skins/blizzard/pvp.lua
@@ -1,96 +1,81 @@
-local E, L, V, P, G = unpack(ElvUI);
-local S = E:GetModule("Skins");
+local E, L, V, P, G = unpack(ElvUI)
+local S = E:GetModule("Skins")
 
-local _G = _G;
-local MAX_ARENA_TEAMS = MAX_ARENA_TEAMS;
+local _G = _G
+local MAX_ARENA_TEAMS = MAX_ARENA_TEAMS
 
-local function LoadSkin()
-	if(E.private.skins.blizzard.enable ~= true or E.private.skins.blizzard.pvp ~= true) then return; end
+function S:LoadPVPSkin()
+	if(E.private.skins.blizzard.enable ~= true or E.private.skins.blizzard.pvp ~= true) then return end
 
-	BattlefieldFrame:StripTextures(true);
-	BattlefieldFrame:CreateBackdrop("Transparent");
-	BattlefieldFrame.backdrop:Point("TOPLEFT", 10, -12);
-	BattlefieldFrame.backdrop:Point("BOTTOMRIGHT", -32, 73);
+	BattlefieldFrame:StripTextures(true)
+	BattlefieldFrame:CreateBackdrop("Transparent")
+	BattlefieldFrame.backdrop:Point("TOPLEFT", 10, -12)
+	BattlefieldFrame.backdrop:Point("BOTTOMRIGHT", -32, 73)
 
-	BattlefieldFrameInfoScrollFrameChildFrameDescription:SetTextColor(1, 1, 1);
-	BattlefieldFrameInfoScrollFrameChildFrameRewardsInfoDescription:SetTextColor(1, 1, 1);
+	-- BattlefieldFrameInfoScrollFrameChildFrameDescription:SetTextColor(1, 1, 1)
+	-- BattlefieldFrameInfoScrollFrameChildFrameRewardsInfoDescription:SetTextColor(1, 1, 1)
 
-	S:HandleButton(BattlefieldFrameCancelButton);
-	S:HandleButton(BattlefieldFrameJoinButton);
-	BattlefieldFrameGroupJoinButton:Point("RIGHT", BattlefieldFrameJoinButton, "LEFT", -2, 0);
-	S:HandleButton(BattlefieldFrameGroupJoinButton);
+	S:HandleButton(BattlefieldFrameCancelButton)
+	S:HandleButton(BattlefieldFrameJoinButton)
+	BattlefieldFrameGroupJoinButton:Point("RIGHT", BattlefieldFrameJoinButton, "LEFT", -2, 0)
+	S:HandleButton(BattlefieldFrameGroupJoinButton)
 
-	S:HandleCloseButton(BattlefieldFrameCloseButton);
+	S:HandleCloseButton(BattlefieldFrameCloseButton)
 
-	PVPBattlegroundFrame:StripTextures(true);
+	-- PVPBattlegroundFrame:StripTextures(true)
 
-	WintergraspTimer:Size(24, 24);
-	WintergraspTimer:SetTemplate("Default");
+	-- PVPBattlegroundFrameTypeScrollFrame:StripTextures()
+	-- S:HandleScrollBar(PVPBattlegroundFrameTypeScrollFrameScrollBar)
 
-	WintergraspTimer.texture:SetDrawLayer("ARTWORK");
-	WintergraspTimer.texture:SetInside();
+	-- S:HandleButton(PVPBattlegroundFrameCancelButton)
 
-	WintergraspTimer:HookScript("OnUpdate", function(self)
-		local canQueue = CanQueueForWintergrasp();
-		if(canQueue) then
-			self.texture:SetTexCoord(0.2, 0.8, 0.6, 0.9);
-		else
-			self.texture:SetTexCoord(0.2, 0.8, 0.1, 0.4);
-		end
-	end);
+	-- PVPBattlegroundFrameInfoScrollFrame:StripTextures()
+	-- S:HandleScrollBar(PVPBattlegroundFrameInfoScrollFrameScrollBar)
 
-	PVPBattlegroundFrameTypeScrollFrame:StripTextures();
-	S:HandleScrollBar(PVPBattlegroundFrameTypeScrollFrameScrollBar);
+	-- PVPBattlegroundFrameInfoScrollFrameChildFrameDescription:SetTextColor(1, 1, 1)
+	-- PVPBattlegroundFrameInfoScrollFrameChildFrameRewardsInfo.description:SetTextColor(1, 1, 1)
 
-	S:HandleButton(PVPBattlegroundFrameCancelButton);
+	-- S:HandleButton(PVPBattlegroundFrameJoinButton)
+	-- PVPBattlegroundFrameGroupJoinButton:Point("RIGHT", PVPBattlegroundFrameJoinButton, "LEFT", -2, 0)
+	-- S:HandleButton(PVPBattlegroundFrameGroupJoinButton)
 
-	PVPBattlegroundFrameInfoScrollFrame:StripTextures();
-	S:HandleScrollBar(PVPBattlegroundFrameInfoScrollFrameScrollBar);
+	-- PVPParentFrame:CreateBackdrop("Transparent")
+	-- PVPParentFrame.backdrop:Point("TOPLEFT", 12, -13)
+	-- PVPParentFrame.backdrop:Point("BOTTOMRIGHT", -30, 76)
 
-	PVPBattlegroundFrameInfoScrollFrameChildFrameDescription:SetTextColor(1, 1, 1);
-	PVPBattlegroundFrameInfoScrollFrameChildFrameRewardsInfo.description:SetTextColor(1, 1, 1);
+	-- S:HandleCloseButton(PVPParentFrameCloseButton)
 
-	S:HandleButton(PVPBattlegroundFrameJoinButton);
-	PVPBattlegroundFrameGroupJoinButton:Point("RIGHT", PVPBattlegroundFrameJoinButton, "LEFT", -2, 0);
-	S:HandleButton(PVPBattlegroundFrameGroupJoinButton);
+	-- PVPFrame:StripTextures(true)
 
-	PVPParentFrame:CreateBackdrop("Transparent");
-	PVPParentFrame.backdrop:Point("TOPLEFT", 12, -13);
-	PVPParentFrame.backdrop:Point("BOTTOMRIGHT", -30, 76);
+	-- for i = 1, MAX_ARENA_TEAMS do
+	-- 	local pvpTeam = _G["PVPTeam" .. i]
+	-- 	pvpTeam:StripTextures()
+	-- 	pvpTeam:CreateBackdrop("Default")
+	-- 	pvpTeam.backdrop:Point("TOPLEFT", 9, -4)
+	-- 	pvpTeam.backdrop:Point("BOTTOMRIGHT", -24, 3)
 
-	S:HandleCloseButton(PVPParentFrameCloseButton);
+	-- 	pvpTeam:HookScript("OnEnter", S.SetModifiedBackdrop)
+	-- 	pvpTeam:HookScript("OnLeave", S.SetOriginalBackdrop)
 
-	PVPFrame:StripTextures(true);
+	-- 	_G["PVPTeam" .. i .. "Highlight"]:Kill()
+	-- end
 
-	for i = 1, MAX_ARENA_TEAMS do
-		local pvpTeam = _G["PVPTeam" .. i];
-		pvpTeam:StripTextures();
-		pvpTeam:CreateBackdrop("Default");
-		pvpTeam.backdrop:Point("TOPLEFT", 9, -4);
-		pvpTeam.backdrop:Point("BOTTOMRIGHT", -24, 3);
+	-- PVPTeamDetails:StripTextures()
+	-- PVPTeamDetails:SetTemplate("Transparent")
 
-		pvpTeam:HookScript("OnEnter", S.SetModifiedBackdrop);
-		pvpTeam:HookScript("OnLeave", S.SetOriginalBackdrop);
+	-- S:HandleCloseButton(PVPTeamDetailsCloseButton)
 
-		_G["PVPTeam" .. i .. "Highlight"]:Kill();
-	end
+	-- for i = 1, 5 do
+	-- 	_G["PVPTeamDetailsFrameColumnHeader" .. i]:StripTextures()
+	-- end
 
-	PVPTeamDetails:StripTextures();
-	PVPTeamDetails:SetTemplate("Transparent");
+	-- S:HandleButton(PVPTeamDetailsAddTeamMember)
 
-	S:HandleCloseButton(PVPTeamDetailsCloseButton);
+	-- S:HandleNextPrevButton(PVPTeamDetailsToggleButton)
 
-	for i = 1, 5 do
-		_G["PVPTeamDetailsFrameColumnHeader" .. i]:StripTextures();
-	end
-
-	S:HandleButton(PVPTeamDetailsAddTeamMember);
-
-	S:HandleNextPrevButton(PVPTeamDetailsToggleButton);
-
-	for i = 1, 2 do
-		S:HandleTab(_G["PVPParentFrameTab" .. i]);
-	end
+	-- for i = 1, 2 do
+	-- 	S:HandleTab(_G["PVPParentFrameTab" .. i])
+	-- end
 end
 
-S:AddCallback("PvP", LoadSkin);
+S:AddCallback("PvP", S.LoadPVPSkin)
