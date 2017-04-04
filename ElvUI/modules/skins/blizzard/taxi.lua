ca2bb51b6dc09cde1b62096419f46c429958d8de
Crum <stschleyer89@gmail.com>
2017-04-04 16:56:38 -0500

    Taxi Skin

--- ElvUI/modules/skins/blizzard/taxi.lua
+++ ElvUI/modules/skins/blizzard/taxi.lua
@@ -1,20 +1,20 @@
-local E, L, V, P, G = unpack(ElvUI);
-local S = E:GetModule("Skins");
+local E, L, V, P, G = unpack(ElvUI)
+local S = E:GetModule("Skins")
 
-local function LoadSkin()
-	if(E.private.skins.blizzard.enable ~= true or E.private.skins.blizzard.taxi ~= true) then return; end
+function S:LoadTaxiSkin()
+	if(E.private.skins.blizzard.enable ~= true or E.private.skins.blizzard.taxi ~= true) then return end
 
-	TaxiFrame:CreateBackdrop("Transparent");
-	TaxiFrame.backdrop:Point("TOPLEFT", 11, -12);
-	TaxiFrame.backdrop:Point("BOTTOMRIGHT", -34, 75);
+	TaxiFrame:CreateBackdrop("Transparent")
+	TaxiFrame.backdrop:Point("TOPLEFT", 11, -12)
+	TaxiFrame.backdrop:Point("BOTTOMRIGHT", -34, 75)
 
-	TaxiFrame:StripTextures();
+	TaxiFrame:StripTextures()
 
-	TaxiPortrait:Kill();
+	TaxiPortrait:Kill()
 
-	S:HandleCloseButton(TaxiCloseButton);
+	S:HandleCloseButton(TaxiCloseButton)
 
-	TaxiRouteMap:CreateBackdrop("Default");
+	TaxiRouteMap:CreateBackdrop("Default")
 end
 
-S:AddCallback("Taxi", LoadSkin);
+S:AddCallback("Taxi", S.LoadTaxiSkin)
