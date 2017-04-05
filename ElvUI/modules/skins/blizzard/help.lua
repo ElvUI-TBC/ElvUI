e50d019dbcf25f7b5fa66683c99b731b8d6aad72
Crum <stschleyer89@gmail.com>
2017-04-05 15:35:43 -0500

    Help Skin

--- ElvUI/modules/skins/blizzard/help.lua
+++ ElvUI/modules/skins/blizzard/help.lua
@@ -1,88 +1,96 @@
-local E, L, V, P, G = unpack(ElvUI);
-local S = E:GetModule("Skins");
+local E, L, V, P, G = unpack(ElvUI)
+local S = E:GetModule("Skins")
 
-local _G = _G;
+local _G = _G
 
-local function LoadSkin()
-	if(E.private.skins.blizzard.enable ~= true or E.private.skins.blizzard.help ~= true) then return; end
+function S:LoadHelpSkin()
+	if(E.private.skins.blizzard.enable ~= true or E.private.skins.blizzard.help ~= true) then return end
+
+	local knowFrameButtons = {
+		"Cancel",
+		"GMTalk",
+		"ReportIssue",
+		"Stuck",
+		"SearchButton",
+		"TopIssuesButton"
+	}
 
 	local helpFrameButtons = {
-		"GMTalkOpenTicket", "GMTalkCancel",
-		"ReportIssueOpenTicket", "ReportIssueCancel",
-		"LagLoot", "LagAuctionHouse", "LagMail", "LagChat", "LagMovement", "LagSpell", "LagCancel",
-		"StuckStuck", "StuckOpenTicket", "StuckCancel",
-		"OpenTicketCancel", "OpenTicketSubmit",
-		"ViewResponseCancel", "ViewResponseMoreHelp", "ViewResponseIssueResolved",
-		"WelcomeGMTalk", "WelcomeReportIssue", "WelcomeStuck", "WelcomeCancel"
-	};
+		"GMTalkOpenTicket",
+		"GMTalkCancel",
+		"OpenTicketSubmit",
+		"OpenTicketCancel",
+		"ReportIssueOpenTicket",
+		"ReportIssueCancel",
+		"StuckStuck",
+		"StuckOpenTicket",
+		"StuckCancel"
+	}
 
-	HelpFrame:StripTextures();
-	HelpFrame:CreateBackdrop("Transparent");
-	HelpFrame.backdrop:Point("TOPLEFT", 6, -6);
-	HelpFrame.backdrop:Point("BOTTOMRIGHT", -45, 14);
+	HelpFrame:StripTextures()
+	HelpFrame:CreateBackdrop("Transparent")
+	HelpFrame.backdrop:Point("TOPLEFT", 6, -6)
+	HelpFrame.backdrop:Point("BOTTOMRIGHT", -45, 14)
 
-	S:HandleCloseButton(HelpFrameCloseButton);
+	S:HandleCloseButton(HelpFrameCloseButton)
 
-	for i = 1, #helpFrameButtons do
-		local button = _G["HelpFrame" .. helpFrameButtons[i]];
-		S:HandleButton(button);
+	for i = 1, #knowFrameButtons do
+		local knowButton = _G["KnowledgeBaseFrame" .. knowFrameButtons[i]]
+		S:HandleButton(knowButton)
 	end
 
-	HelpFrameOpenTicketDivider:StripTextures();
+	for i = 1, #helpFrameButtons do
+		local helpButton = _G["HelpFrame" .. helpFrameButtons[i]]
+		S:HandleButton(helpButton)
+	end
 
-	S:HandleScrollBar(HelpFrameOpenTicketScrollFrameScrollBar);
+		-- hide header textures and move text/buttons.
+	local BlizzardHeader = {
+		"KnowledgeBaseFrame"
+	}
 
-	HelpFrameOpenTicketSubmit:SetPoint("RIGHT", HelpFrameOpenTicketCancel, "LEFT", -2, 0);
+	for i = 1, #BlizzardHeader do
+		local title = _G[BlizzardHeader[i].."Header"]
+		if title then
+			title:SetTexture("")
+			title:ClearAllPoints()
+			if title == _G["GameMenuFrameHeader"] then
+				title:SetPoint("TOP", GameMenuFrame, 0, 0)
+			else
+				title:SetPoint("TOP", BlizzardHeader[i], -22, -8)
+			end
+		end
+	end
 
-	S:HandleScrollBar(HelpFrameViewResponseIssueScrollFrameScrollBar);
-	HelpFrameViewResponseDivider:Kill();
-	S:HandleScrollBar(HelpFrameViewResponseMessageScrollFrameScrollBar);
-	HelpFrameViewResponseIssueResolved:SetPoint("LEFT", HelpFrameViewResponseMoreHelp, "RIGHT", -3, 0);
+	HelpFrameOpenTicketDivider:StripTextures()
 
-	KnowledgeBaseFrame:StripTextures();
+	S:HandleScrollBar(HelpFrameOpenTicketScrollFrame)
+	S:HandleScrollBar(HelpFrameOpenTicketScrollFrameScrollBar)
+	S:HandleScrollBar(KnowledgeBaseArticleScrollFrameScrollBar)
 
-	KnowledgeBaseFrame:HookScript("OnShow", function()
-		select(11, HelpFrame:GetRegions()):Hide();
-	end);
+	HelpFrameOpenTicketSubmit:SetPoint("RIGHT", HelpFrameOpenTicketCancel, "LEFT", -2, 0)
+	KnowledgeBaseFrameStuck:SetPoint("LEFT", KnowledgeBaseFrameReportIssue, "RIGHT", 2, 0)
 
-	KnowledgeBaseFrame:SetScript("OnHide", function()
-		select(11, HelpFrame:GetRegions()):Show();
-	end);
+	KnowledgeBaseFrame:StripTextures()
 
-	S:HandleButton(KnowledgeBaseFrameTopIssuesButton);
-	S:HandleButton(GMChatOpenLog);
+	KnowledgeBaseFrameDivider:Kill()
+	KnowledgeBaseFrameDivider2:Kill()
 
-	KnowledgeBaseFrameDivider:Kill();
+	S:HandleEditBox(KnowledgeBaseFrameEditBox)
 
-	S:HandleEditBox(KnowledgeBaseFrameEditBox);
-	KnowledgeBaseFrameEditBox.backdrop:Point("TOPLEFT", -E.Border, -4);
-	KnowledgeBaseFrameEditBox.backdrop:Point("BOTTOMRIGHT", E.Border, 7);
+	KnowledgeBaseFrameEditBox.backdrop:Point("TOPLEFT", -E.Border, -4)
+	KnowledgeBaseFrameEditBox.backdrop:Point("BOTTOMRIGHT", E.Border, 7)
 
-	S:HandleDropDownBox(KnowledgeBaseFrameCategoryDropDown);
-	S:HandleDropDownBox(KnowledgeBaseFrameSubCategoryDropDown);
+	S:HandleDropDownBox(KnowledgeBaseFrameCategoryDropDown)
+	KnowledgeBaseFrameCategoryDropDown:Point("TOPLEFT", "KnowledgeBaseFrameEditBox", "TOPRIGHT", -14, -3)
+	S:HandleDropDownBox(KnowledgeBaseFrameSubCategoryDropDown)
+	KnowledgeBaseFrameSubCategoryDropDown:Point("TOPLEFT", "KnowledgeBaseFrameCategoryDropDown","TOPRIGHT", -24, 0)
+	KnowledgeBaseFrameSearchButton:Point("TOPLEFT", "KnowledgeBaseFrameSubCategoryDropDown","TOPRIGHT", -4, -2)
 
-	S:HandleButton(KnowledgeBaseFrameSearchButton);
+	S:HandleNextPrevButton(KnowledgeBaseArticleListFrameNextButton)
+	S:HandleNextPrevButton(KnowledgeBaseArticleListFramePreviousButton)
 
-	KnowledgeBaseFrameDivider2:Kill();
-
-	S:HandleNextPrevButton(KnowledgeBaseArticleListFrameNextButton);
-	S:HandleNextPrevButton(KnowledgeBaseArticleListFramePreviousButton);
-
-	S:HandleScrollBar(KnowledgeBaseArticleScrollFrameScrollBar);
-	S:HandleButton(KnowledgeBaseArticleScrollChildFrameBackButton);
-
-	S:HandleButton(KnowledgeBaseFrameReportIssue);
-	KnowledgeBaseFrameGMTalk:SetPoint("BOTTOM", KnowledgeBaseFrameReportIssue, "TOP", 0, 2);
-	S:HandleButton(KnowledgeBaseFrameGMTalk);
-	S:HandleButton(KnowledgeBaseFrameAbandonTicket);
-	KnowledgeBaseFrameEditTicket:SetPoint("BOTTOM", KnowledgeBaseFrameAbandonTicket, "TOP", 0, 2);
-	S:HandleButton(KnowledgeBaseFrameEditTicket);
-
-	KnowledgeBaseFrameStuck:SetPoint("LEFT", KnowledgeBaseFrameReportIssue, "RIGHT", 2, 0);
-	S:HandleButton(KnowledgeBaseFrameStuck);
-	KnowledgeBaseFrameLag:SetPoint("LEFT", KnowledgeBaseFrameGMTalk, "RIGHT", 2, 0);
-	S:HandleButton(KnowledgeBaseFrameLag);
-	S:HandleButton(KnowledgeBaseFrameCancel);
+	S:HandleButton(KnowledgeBaseArticleScrollChildFrameBackButton)
 end
 
-S:AddCallback("Help", LoadSkin);
+S:AddCallback("Help", S.LoadHelpSkin)
