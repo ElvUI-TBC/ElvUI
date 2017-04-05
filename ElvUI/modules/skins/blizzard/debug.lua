e3a8567421cdaa9de123e71669619c88b8f18483
Crum <stschleyer89@gmail.com>
2017-04-05 13:48:48 -0500

    Debug Skin update

--- ElvUI/modules/skins/blizzard/debug.lua
+++ ElvUI/modules/skins/blizzard/debug.lua
@@ -1,64 +1,64 @@
-local E, L, V, P, G = unpack(ElvUI);
-local S = E:GetModule("Skins");
+local E, L, V, P, G = unpack(ElvUI)
+local S = E:GetModule("Skins")
 
-local function LoadSkin()
-	if E.private.skins.blizzard.enable ~= true or E.private.skins.blizzard.debug ~= true then return end
+function S:LoadDebugSkin()
+  if E.private.skins.blizzard.enable ~= true or E.private.skins.blizzard.debug ~= true then return end
 
-	ScriptErrorsFrame:SetParent(E.UIParent)
-	ScriptErrorsFrame:SetTemplate("Transparent")
-	S:HandleScrollBar(ScriptErrorsFrameScrollFrameScrollBar)
-	S:HandleCloseButton(ScriptErrorsFrameClose)
-	ScriptErrorsFrameScrollFrameText:FontTemplate(nil, 13)
-	ScriptErrorsFrameScrollFrame:CreateBackdrop("Default")
-	ScriptErrorsFrameScrollFrame:SetFrameLevel(ScriptErrorsFrameScrollFrame:GetFrameLevel() + 2)
-	EventTraceFrame:SetTemplate("Transparent")
+  ScriptErrorsFrame:SetParent(E.UIParent)
+  ScriptErrorsFrame:SetTemplate("Transparent")
+  S:HandleScrollBar(ScriptErrorsFrameScrollFrameScrollBar)
+  S:HandleCloseButton(ScriptErrorsFrameClose)
+  ScriptErrorsFrameScrollFrameText:FontTemplate(nil, 13)
+  ScriptErrorsFrameScrollFrame:CreateBackdrop("Default")
+  ScriptErrorsFrameScrollFrame:SetFrameLevel(ScriptErrorsFrameScrollFrame:GetFrameLevel() + 2)
+  EventTraceFrame:SetTemplate("Transparent")
 
-	local texs = {
-		"TopLeft",
-		"TopRight",
-		"Top",
-		"BottomLeft",
-		"BottomRight",
-		"Bottom",
-		"Left",
-		"Right",
-		"TitleBG",
-		"DialogBG",
-	}
+  local texs = {
+    "TopLeft",
+    "TopRight",
+    "Top",
+    "BottomLeft",
+    "BottomRight",
+    "Bottom",
+    "Left",
+    "Right",
+    "TitleBG",
+    "DialogBG",
+  }
 
-	for i = 1, #texs do
-		_G["ScriptErrorsFrame"..texs[i]]:SetTexture(nil)
-		_G["EventTraceFrame"..texs[i]]:SetTexture(nil)
-	end
+  for i = 1, #texs do
+    _G["ScriptErrorsFrame"..texs[i]]:SetTexture(nil)
+    _G["EventTraceFrame"..texs[i]]:SetTexture(nil)
+  end
 
-	S:HandleButton(ScriptErrorsFrame.reload)
-	S:HandleButton(ScriptErrorsFrame.firstButton)
-	S:HandleButton(ScriptErrorsFrame.lastButton)
-	S:HandleNextPrevButton(ScriptErrorsFrame.previous)
-	S:HandleNextPrevButton(ScriptErrorsFrame.next)
-	S:HandleButton(ScriptErrorsFrame.close)
+  S:HandleButton(ScriptErrorsFrame.reload)
+  S:HandleButton(ScriptErrorsFrame.firstButton)
+  S:HandleButton(ScriptErrorsFrame.lastButton)
+  S:HandleNextPrevButton(ScriptErrorsFrame.previous)
+  S:HandleNextPrevButton(ScriptErrorsFrame.next)
+  S:HandleButton(ScriptErrorsFrame.close)
 
-	-- TODO FIX HandleNextPrevButton button size
-	ScriptErrorsFrame.previous:Point("BOTTOM", ScriptErrorsFrame, "BOTTOM", -50, 12)
-	ScriptErrorsFrame.next:Point("BOTTOM", ScriptErrorsFrame, "BOTTOM", 50, 12)
+  -- TODO FIX HandleNextPrevButton button size
+  ScriptErrorsFrame.previous:Point("BOTTOM", ScriptErrorsFrame, "BOTTOM", -50, 12)
+  ScriptErrorsFrame.next:Point("BOTTOM", ScriptErrorsFrame, "BOTTOM", 50, 12)
 
-	local noscalemult = E.mult * GetCVar("uiScale")
-	FrameStackTooltip:HookScript("OnShow", function(self)
-		self:SetBackdrop({
-			bgFile = E["media"].blankTex,
-			edgeFile = E["media"].blankTex,
-			tile = false, tileSize = 0, edgeSize = noscalemult,
-			insets = { left = -noscalemult, right = -noscalemult, top = -noscalemult, bottom = -noscalemult}
-		});
-		self:SetBackdropColor(unpack(E["media"].backdropfadecolor))
-		self:SetBackdropBorderColor(unpack(E["media"].bordercolor))
-	end)
+  local noscalemult = E.mult * GetCVar("uiScale")
+  FrameStackTooltip:HookScript("OnShow", function(self)
+    self:SetBackdrop({
+      bgFile = E["media"].blankTex,
+      edgeFile = E["media"].blankTex,
+      tile = false, tileSize = 0, edgeSize = noscalemult,
+      insets = { left = -noscalemult, right = -noscalemult, top = -noscalemult, bottom = -noscalemult}
+    })
+    self:SetBackdropColor(unpack(E["media"].backdropfadecolor))
+    self:SetBackdropBorderColor(unpack(E["media"].bordercolor))
+  end)
 
-	EventTraceTooltip:HookScript("OnShow", function(self)
-		self:SetTemplate("Transparent")
-	end)
+  EventTraceTooltip:HookScript("OnShow", function(self)
+    self:SetTemplate("Transparent")
+  end)
 
-	S:HandleCloseButton(EventTraceFrameCloseButton)
+  S:HandleCloseButton(EventTraceFrameCloseButton)
 end
 
-S:AddCallbackForAddon("!DebugTools", "DebugTools", LoadSkin);
+S:AddCallbackForAddon("!DebugTools", "DebugTools", S.LoadDebugSkin)
