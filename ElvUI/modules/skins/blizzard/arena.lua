d7fbb8b9741e33f893d5f680fa66a06e8e120065
Crum <stschleyer89@gmail.com>
2017-04-04 20:59:17 -0500

    Arena Skin

--- ElvUI/modules/skins/blizzard/arena.lua
+++ ElvUI/modules/skins/blizzard/arena.lua
@@ -1,23 +1,23 @@
-local E, L, V, P, G = unpack(ElvUI);
-local S = E:GetModule("Skins");
+local E, L, V, P, G = unpack(ElvUI)
+local S = E:GetModule("Skins")
 
-local function LoadSkin()
-	if(E.private.skins.blizzard.enable ~= true or E.private.skins.blizzard.arena ~= true) then return; end
+function S:LoadArenaSkin()
+	if(E.private.skins.blizzard.enable ~= true or E.private.skins.blizzard.arena ~= true) then return end
 
-	ArenaFrame:CreateBackdrop("Transparent");
-	ArenaFrame.backdrop:Point("TOPLEFT", 11, -12);
-	ArenaFrame.backdrop:Point("BOTTOMRIGHT", -34, 74);
+	ArenaFrame:CreateBackdrop("Transparent")
+	ArenaFrame.backdrop:Point("TOPLEFT", 11, -12)
+	ArenaFrame.backdrop:Point("BOTTOMRIGHT", -34, 74)
 
-	ArenaFrame:StripTextures(true);
+	ArenaFrame:StripTextures(true)
 
-	ArenaFrameZoneDescription:SetTextColor(1, 1, 1);
+	ArenaFrameZoneDescription:SetTextColor(1, 1, 1)
 
-	S:HandleButton(ArenaFrameCancelButton);
-	S:HandleButton(ArenaFrameJoinButton);
-	S:HandleButton(ArenaFrameGroupJoinButton);
-	ArenaFrameGroupJoinButton:Point("RIGHT", ArenaFrameJoinButton, "LEFT", -2, 0);
+	S:HandleButton(ArenaFrameCancelButton)
+	S:HandleButton(ArenaFrameJoinButton)
+	S:HandleButton(ArenaFrameGroupJoinButton)
+	ArenaFrameGroupJoinButton:Point("RIGHT", ArenaFrameJoinButton, "LEFT", -2, 0)
 
-	S:HandleCloseButton(ArenaFrameCloseButton);
+	S:HandleCloseButton(ArenaFrameCloseButton)
 end
 
-S:AddCallback("Arena", LoadSkin);
+S:AddCallback("Arena", S.LoadArenaSkin)
