b6f1ee8fe94cbd5165fdde18152377f604d50ccf
Crum <stschleyer89@gmail.com>
2017-04-17 22:34:07 -0500

    Revert "add strupper & stlower"
    This reverts commit 311980b011bae964ba052b53239959cbd5f72e8a.

--- !Compatibility/api/wowAPI.lua
+++ !Compatibility/api/wowAPI.lua
@@ -1,8 +1,6 @@
 --Cache global variables
 local date = date
 local gsub = string.gsub
-local strupper = string.upper
-local strlower = string.lower
 --WoW API
 local GetQuestGreenRange = GetQuestGreenRange
 local UnitBuff = UnitBuff
