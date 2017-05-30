ffc0d64c79bc0c166d14629d756df961de3da926
Crum <stschleyer89@gmail.com>
2017-05-29 20:07:26 -0500

    Revert "update Merchant skin"
    This reverts commit 42b8176eafd63cefccaa8bb77287142633537073.

--- ElvUI/modules/skins/blizzard/merchant.lua
+++ ElvUI/modules/skins/blizzard/merchant.lua
@@ -11,7 +11,7 @@ function S:LoadMerchantSkin()
 
 	S:HandleCloseButton(MerchantFrameCloseButton, MerchantFrame.backdrop)
 
-	for i = 1, MERCHANT_ITEMS_PER_PAGE do
+	for i = 1, 12 do
 		local item = _G["MerchantItem" .. i]
 		local itemButton = _G["MerchantItem" .. i .. "ItemButton"]
 		local iconTexture = _G["MerchantItem" .. i .. "ItemButtonIconTexture"]
@@ -73,7 +73,7 @@ function S:LoadMerchantSkin()
 		local numMerchantItems = GetMerchantNumItems()
 		local index
 		local itemButton, itemName
-		for i = 1, MERCHANT_ITEMS_PER_PAGE do
+		for i = 1, BUYBACK_ITEMS_PER_PAGE do
 			index = (((MerchantFrame.page - 1) * MERCHANT_ITEMS_PER_PAGE) + i)
 			itemButton = _G["MerchantItem" .. i .. "ItemButton"]
 			itemName = _G["MerchantItem" .. i .. "Name"]
