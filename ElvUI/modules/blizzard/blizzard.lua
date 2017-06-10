5988f991aeb126ccc96922e0d341d4b37004d303
Pinya <800pin.ru@gmail.com>
2017-06-10 14:56:12 +0300

    update

--- ElvUI/modules/blizzard/blizzard.lua
+++ ElvUI/modules/blizzard/blizzard.lua
@@ -10,19 +10,14 @@ function B:Initialize()
 	self:PositionDurabilityFrame()
 	self:PositionGMFrames()
 
-	if (GetLocale() == "deDE") then
-		DAY_ONELETTER_ABBR = "%d d"
-		MINUTE_ONELETTER_ABBR = "%d m"
-	end
-
-	QuestLogFrame:HookScript("OnShow", function()
-		local questFrame = QuestLogFrame:GetFrameLevel()
-		local scrollFrame = QuestLogDetailScrollFrame:GetFrameLevel()
-
-		if questFrame >= scrollFrame then
-			QuestLogDetailScrollFrame:SetFrameLevel(questFrame + 1)
-		end
-	end)
+--	QuestLogFrame:HookScript("OnShow", function()
+--		local questFrame = QuestLogFrame:GetFrameLevel()
+--		local scrollFrame = QuestLogDetailScrollFrame:GetFrameLevel()
+--
+--		if questFrame >= scrollFrame then
+--			QuestLogDetailScrollFrame:SetFrameLevel(questFrame + 1)
+--		end
+--	end)
 end
 
 E:RegisterModule(B:GetName())
