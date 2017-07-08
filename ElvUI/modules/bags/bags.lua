ad681999a81be4bd961afa153654ce147eaddb00
Pinya <800pin.ru@gmail.com>
2017-07-08 13:32:11 +0300

    Bags: hook ToggleBackpack func

--- ElvUI/modules/bags/bags.lua
+++ ElvUI/modules/bags/bags.lua
@@ -1256,6 +1256,7 @@ function B:Initialize()
 	--Hook onto Blizzard Functions
 	self:SecureHook("ToggleBackpack");
 	self:SecureHook("ToggleBag", "ToggleBags");
+	self:SecureHook("OpenAllBags", "ToggleBackpack");
 	self:SecureHook("OpenBackpack", "OpenBags");
 	self:SecureHook("CloseAllBags", "CloseBags");
 	self:SecureHook("CloseBackpack", "CloseBags");
