0b0dee935f5dbb9133ba15caeb3c55495097e1e1
Pinya <800pin.ru@gmail.com>
2017-07-08 13:34:11 +0300

    fix Honor reward position in QuestFrame and QuestLogFrame

--- ElvUI/modules/blizzard/blizzard.lua
+++ ElvUI/modules/blizzard/blizzard.lua
@@ -1,6 +1,15 @@
 local E, L, V, P, G = unpack(ElvUI)
 local B = E:NewModule("Blizzard", "AceEvent-3.0", "AceHook-3.0")
 
+local GetNumQuestChoices = GetNumQuestChoices
+local GetNumQuestLogChoices = GetNumQuestLogChoices
+local GetQuestLogRewardHonor = GetQuestLogRewardHonor
+local GetQuestLogRewardMoney = GetQuestLogRewardMoney
+local GetQuestLogRewardSpell = GetQuestLogRewardSpell
+local GetRewardHonor = GetRewardHonor
+local GetRewardMoney = GetRewardMoney
+local GetRewardSpell = GetRewardSpell
+
 E.Blizzard = B
 
 function B:Initialize()
@@ -16,6 +25,41 @@ function B:Initialize()
 		if not CHATCONFIG_SELECTED_FILTER_SETTINGS then return end
 		self.hooks.CombatConfig_Colorize_Update()
 	end, true)
+
+	hooksecurefunc("QuestFrameItems_Update", function(questState)
+		local spacerFrame, money, honor, numQuestChoices, numQuestSpellRewards
+
+		if questState == "QuestLog" then
+			spacerFrame = QuestLogSpacerFrame
+			money, honor, numQuestChoices, numQuestSpellRewards = GetQuestLogRewardMoney(), GetQuestLogRewardHonor(), GetNumQuestLogChoices(), GetQuestLogRewardSpell()
+		else
+			spacerFrame = QuestSpacerFrame
+			money, honor, numQuestChoices, numQuestSpellRewards = GetRewardMoney(), GetRewardHonor(), GetNumQuestChoices(), GetRewardSpell()
+		end
+
+		if money == 0 and honor > 0 and (numQuestChoices > 0 or numQuestSpellRewards) then
+			numQuestSpellRewards = numQuestSpellRewards and 1 or 0
+			local rewardsCount = numQuestChoices + numQuestSpellRewards
+
+			local questItemReceiveText = _G[questState.."ItemReceiveText"]
+			if numQuestSpellRewards > 0 then
+				questItemReceiveText:SetText(REWARD_ITEMS)
+				questItemReceiveText:SetPoint("TOPLEFT", questState.."Item"..rewardsCount, "BOTTOMLEFT", 3, 15)
+			elseif numQuestChoices > 0 then
+				questItemReceiveText:SetText(REWARD_ITEMS)
+				local index = numQuestChoices
+				if mod(index, 2) == 0 then
+					index = index - 1
+				end
+				questItemReceiveText:SetPoint("TOPLEFT", questState.."Item"..index, "BOTTOMLEFT", 3, 15)
+			else
+				questItemReceiveText:SetText(REWARD_ITEMS_ONLY)
+				questItemReceiveText:SetPoint("TOPLEFT", questState.."RewardTitleText", "BOTTOMLEFT", 3, 15)
+			end
+
+			QuestFrame_SetAsLastShown(questItemReceiveText, spacerFrame)
+		end
+	end)
 
 --	QuestLogFrame:HookScript("OnShow", function()
 --		local questFrame = QuestLogFrame:GetFrameLevel()
