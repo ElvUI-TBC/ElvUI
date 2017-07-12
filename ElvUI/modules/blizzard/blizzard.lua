3faa0b10cb57c92151ff60b2e0d81d9b53ad7719
Pinya <800pin.ru@gmail.com>
2017-07-12 03:31:01 +0300

    update QuestLog Honor fix

--- ElvUI/modules/blizzard/blizzard.lua
+++ ElvUI/modules/blizzard/blizzard.lua
@@ -27,37 +27,49 @@ function B:Initialize()
 	end, true)
 
 	hooksecurefunc("QuestFrameItems_Update", function(questState)
-		local spacerFrame, money, honor, numQuestChoices, numQuestSpellRewards
+		local spacerFrame, money, honor, numQuestRewards, numQuestChoices, numQuestSpellRewards
 
 		if questState == "QuestLog" then
 			spacerFrame = QuestLogSpacerFrame
-			money, honor, numQuestChoices, numQuestSpellRewards = GetQuestLogRewardMoney(), GetQuestLogRewardHonor(), GetNumQuestLogChoices(), GetQuestLogRewardSpell()
+			money, honor, numQuestRewards, numQuestChoices, numQuestSpellRewards = GetQuestLogRewardMoney(), GetQuestLogRewardHonor(), GetNumQuestLogRewards(), GetNumQuestLogChoices(), GetQuestLogRewardSpell()
 		else
 			spacerFrame = QuestSpacerFrame
-			money, honor, numQuestChoices, numQuestSpellRewards = GetRewardMoney(), GetRewardHonor(), GetNumQuestChoices(), GetRewardSpell()
+			money, honor, numQuestRewards, numQuestChoices, numQuestSpellRewards = GetRewardMoney(), GetRewardHonor(), GetNumQuestRewards(), GetNumQuestChoices(), GetRewardSpell()
 		end
 
-		if money == 0 and honor > 0 and (numQuestChoices > 0 or numQuestSpellRewards) then
+		if money == 0 and honor > 0 and (numQuestRewards > 0 or numQuestChoices > 0 or numQuestSpellRewards) then
 			numQuestSpellRewards = numQuestSpellRewards and 1 or 0
-			local rewardsCount = numQuestChoices + numQuestSpellRewards
+			local rewardsCount = numQuestRewards + numQuestChoices + numQuestSpellRewards
+			local honorFrame = _G[questState.."HonorFrame"]
 
-			local questItemReceiveText = _G[questState.."ItemReceiveText"]
-			if numQuestSpellRewards > 0 then
-				questItemReceiveText:SetText(REWARD_ITEMS)
-				questItemReceiveText:SetPoint("TOPLEFT", questState.."Item"..rewardsCount, "BOTTOMLEFT", 3, 15)
-			elseif numQuestChoices > 0 then
-				questItemReceiveText:SetText(REWARD_ITEMS)
-				local index = numQuestChoices
-				if mod(index, 2) == 0 then
-					index = index - 1
-				end
-				questItemReceiveText:SetPoint("TOPLEFT", questState.."Item"..index, "BOTTOMLEFT", 3, 15)
+			if numQuestRewards > 0 then
+				honorFrame:ClearAllPoints()
+				honorFrame:SetPoint("TOPLEFT", questState.."Item"..rewardsCount, "BOTTOMLEFT", 3, 0)
+
+				QuestFrame_SetAsLastShown(questState.."HonorFrame", spacerFrame)
 			else
-				questItemReceiveText:SetText(REWARD_ITEMS_ONLY)
-				questItemReceiveText:SetPoint("TOPLEFT", questState.."RewardTitleText", "BOTTOMLEFT", 3, 15)
-			end
+				local questItemReceiveText = _G[questState.."ItemReceiveText"]
+				honorFrame:ClearAllPoints()
+				honorFrame:SetPoint("TOPLEFT", questItemReceiveText, "BOTTOMLEFT", 0, -5)
 
-			QuestFrame_SetAsLastShown(questItemReceiveText, spacerFrame)
+				if numQuestSpellRewards > 0 then
+					questItemReceiveText:SetText(REWARD_ITEMS)
+					questItemReceiveText:SetPoint("TOPLEFT", questState.."Item"..rewardsCount, "BOTTOMLEFT", 3, 15)
+				elseif numQuestChoices > 0 then
+					questItemReceiveText:SetText(REWARD_ITEMS)
+					local index = numQuestChoices
+					if mod(index, 2) == 0 then
+						index = index - 1
+					end
+
+					questItemReceiveText:SetPoint("TOPLEFT", questState.."Item"..index, "BOTTOMLEFT", 3, 15)
+				else
+					questItemReceiveText:SetText(REWARD_ITEMS_ONLY)
+					questItemReceiveText:SetPoint("TOPLEFT", questState.."RewardTitleText", "BOTTOMLEFT", 3, 15)
+				end
+
+				QuestFrame_SetAsLastShown(questItemReceiveText, spacerFrame)
+			end
 		end
 	end)
 
