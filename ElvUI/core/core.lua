4a38a00505dd284c9355b372c8e9de258c6ce052
Appolyonn <jellefenxd@gmail.com>
2017-07-29 20:02:37 +0300

    oops

--- ElvUI/core/core.lua
+++ ElvUI/core/core.lua
@@ -1105,6 +1105,6 @@ function E:Initialize()
 
 	if(self.db.general.loginmessage) then
 	--	print(select(2, E:GetModule("Chat"):FindURL("CHAT_MSG_DUMMY", format(L["LOGIN_MSG"], self["media"].hexvaluecolor, self["media"].hexvaluecolor, self.version)))..".")
-		print(select(format(L["LOGIN_MSG"], self["media"].hexvaluecolor, self["media"].hexvaluecolor, self.version))..".")
+		print(format(L["LOGIN_MSG"], self["media"].hexvaluecolor, self["media"].hexvaluecolor, self.version)..".")
 	end
 end
