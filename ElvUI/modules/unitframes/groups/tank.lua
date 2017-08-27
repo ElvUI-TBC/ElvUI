bf9637196001cce42d4b7b2fa6f077a4160b7938
Bunny67 <zolov67@yandex.ru>
2017-08-27 20:26:35 +0300

    update party, raidPets and tank frames

--- ElvUI/modules/unitframes/groups/tank.lua
+++ ElvUI/modules/unitframes/groups/tank.lua
@@ -1,178 +1,181 @@
-local E, L, V, P, G = unpack(ElvUI);
-local UF = E:GetModule("UnitFrames");
+local E, L, V, P, G = unpack(ElvUI)
+local UF = E:GetModule("UnitFrames")
 
-local RegisterStateDriver = RegisterStateDriver;
-local InCombatLockdown = InCombatLockdown;
+--Cache global variables
+--Lua functions
+local max = math.max
+--WoW API / Variables
+local CreateFrame = CreateFrame
+local InCombatLockdown = InCombatLockdown
+local RegisterStateDriver = RegisterStateDriver
 
-local ns = oUF;
-local ElvUF = ns.oUF;
-assert(ElvUF, "ElvUI was unable to locate oUF.");
+function UF:Construct_TankFrames()
+	self:SetScript("OnEnter", UnitFrame_OnEnter)
+	self:SetScript("OnLeave", UnitFrame_OnLeave)
 
-function UF:Construct_TankFrames(unitGroup)
-	self:SetScript("OnEnter", UnitFrame_OnEnter);
-	self:SetScript("OnLeave", UnitFrame_OnLeave);
+	self.RaisedElementParent = CreateFrame("Frame", nil, self)
+	self.RaisedElementParent.TextureParent = CreateFrame("Frame", nil, self.RaisedElementParent)
+	self.RaisedElementParent:SetFrameLevel(self:GetFrameLevel() + 100)
 
-	self.RaisedElementParent = CreateFrame("Frame", nil, self);
-	self.RaisedElementParent:SetFrameLevel(self:GetFrameLevel() + 100);
+	self.Health = UF:Construct_HealthBar(self, true)
+	self.Name = UF:Construct_NameText(self)
+	self.ThreatIndicator = UF:Construct_Threat(self)
+	self.RaidTargetIndicator = UF:Construct_RaidIcon(self)
+	self.Range = UF:Construct_Range(self)
 
-	self.Health = UF:Construct_HealthBar(self, true);
-	self.Name = UF:Construct_NameText(self);
-	self.ThreatIndicator = UF:Construct_Threat(self);
-	self.RaidTargetIndicator = UF:Construct_RaidIcon(self);
-	self.Range = UF:Construct_Range(self);
+	if not self.isChild then
+		self.Buffs = UF:Construct_Buffs(self)
+		self.Debuffs = UF:Construct_Debuffs(self)
+		self.AuraWatch = UF:Construct_AuraWatch(self)
+		self.RaidDebuffs = UF:Construct_RaidDebuffs(self)
+		self.DebuffHighlight = UF:Construct_DebuffHighlight(self)
 
-	if(not self.isChild) then
-		self.Buffs = UF:Construct_Buffs(self);
-		self.Debuffs = UF:Construct_Debuffs(self);
-		self.AuraWatch = UF:Construct_AuraWatch(self);
-		self.RaidDebuffs = UF:Construct_RaidDebuffs(self);
-		self.DebuffHighlight = UF:Construct_DebuffHighlight(self);
-		self.unitframeType = "tank";
+		self.unitframeType = "tank"
 	else
-		self.unitframeType = "tanktarget";
+		self.unitframeType = "tanktarget"
 	end
 
-	UF:Update_StatusBars();
-	UF:Update_FontStrings();
+	UF:Update_TankFrames(self, E.db["unitframe"]["units"]["tank"])
+	UF:Update_StatusBars()
+	UF:Update_FontStrings()
 
-	self.originalParent = self:GetParent();
+	self.originalParent = self:GetParent()
 
-	UF:Update_TankFrames(self, UF.db["units"]["tank"]);
-
-	return self;
+	return self
 end
 
 function UF:Update_TankHeader(header, db)
-	header:Hide();
-	header.db = db;
+	header:Hide()
+	header.db = db
 
-	UF:ClearChildPoints(header:GetChildren());
+	UF:ClearChildPoints(header:GetChildren())
 
-	header:SetAttribute("startingIndex", -1);
+	header:SetAttribute("startingIndex", -1)
 	RegisterStateDriver(header, "visibility", "show")
-	RegisterStateDriver(header, "visibility", "[target=raid1,exists] show;hide");
-	header:SetAttribute("startingIndex", 1);
+	RegisterStateDriver(header, "visibility", "[target=raid1,exists] showhide")
+	header:SetAttribute("startingIndex", 1)
 
-	header:SetAttribute("point", "BOTTOM");
-	header:SetAttribute("columnAnchorPoint", "LEFT");
+	header:SetAttribute("point", "BOTTOM")
+	header:SetAttribute("columnAnchorPoint", "LEFT")
 
-	UF:ClearChildPoints(header:GetChildren());
-	header:SetAttribute("yOffset", db.verticalSpacing);
+	UF:ClearChildPoints(header:GetChildren())
+	header:SetAttribute("yOffset", db.verticalSpacing)
 
-	local width, height = header:GetSize();
-	header.dirtyWidth, header.dirtyHeight = width, max(height, 2*db.height + db.verticalSpacing);
+	local width, height = header:GetSize()
+	header.dirtyWidth, header.dirtyHeight = width, max(height, 2*db.height + db.verticalSpacing)
 
-	if(not header.positioned) then
-		header:ClearAllPoints();
-		header:Point("TOPLEFT", E.UIParent, "TOPLEFT", 4, -186);
-
-		E:CreateMover(header, header:GetName().."Mover", L["MT Frames"], nil, nil, nil, "ALL,RAID");
-		header.mover.positionOverride = "TOPLEFT";
-		header:SetAttribute("minHeight", header.dirtyHeight);
-		header:SetAttribute("minWidth", header.dirtyWidth);
-		header.positioned = true;
+	if not header.positioned then
+		header:ClearAllPoints()
+		header:Point("TOPLEFT", E.UIParent, "TOPLEFT", 4, -186)
+		E:CreateMover(header, header:GetName().."Mover", L["MT Frames"], nil, nil, nil, "ALL,RAID")
+		header.mover.positionOverride = "TOPLEFT"
+		header:SetAttribute("minHeight", header.dirtyHeight)
+		header:SetAttribute("minWidth", header.dirtyWidth)
+		header.positioned = true
 	end
 end
 
 function UF:Update_TankFrames(frame, db)
-	frame.db = db;
+	frame.db = db
+	frame.colors = ElvUF.colors
+	frame:RegisterForClicks(self.db.targetOnMouseDown and "AnyDown" or "AnyUp")
 
 	do
-		frame.ORIENTATION = db.orientation;
+		frame.ORIENTATION = db.orientation --allow this value to change when unitframes position changes on screen?
+
 		if(self.thinBorders) then
-			frame.SPACING = 0;
-			frame.BORDER = E.mult;
+			frame.SPACING = 0
+			frame.BORDER = E.mult
 		else
-			frame.BORDER = E.Border;
-			frame.SPACING = E.Spacing;
+			frame.BORDER = E.Border
+			frame.SPACING = E.Spacing
 		end
-		frame.SHADOW_SPACING = 3;
+		frame.SHADOW_SPACING = 3
 
-		frame.UNIT_WIDTH = db.width;
-		frame.UNIT_HEIGHT = db.height;
+		frame.UNIT_WIDTH = db.width
+		frame.UNIT_HEIGHT = db.height
 
-		frame.USE_POWERBAR = false;
-		frame.POWERBAR_DETACHED = false;
-		frame.USE_INSET_POWERBAR = false;
-		frame.USE_MINI_POWERBAR = false;
-		frame.USE_POWERBAR_OFFSET = false;
-		frame.POWERBAR_OFFSET = 0;
-		frame.POWERBAR_HEIGHT = 0;
-		frame.POWERBAR_WIDTH = 0;
+		frame.USE_POWERBAR = false
+		frame.POWERBAR_DETACHED = false
+		frame.USE_INSET_POWERBAR = false
+		frame.USE_MINI_POWERBAR = false
+		frame.USE_POWERBAR_OFFSET = false
+		frame.POWERBAR_OFFSET = 0
+		frame.POWERBAR_HEIGHT = 0
+		frame.POWERBAR_WIDTH = 0
 
-		frame.USE_PORTRAIT = false;
-		frame.USE_PORTRAIT_OVERLAY = false;
-		frame.PORTRAIT_WIDTH = 0;
+		frame.USE_PORTRAIT = false
+		frame.USE_PORTRAIT_OVERLAY = false
+		frame.PORTRAIT_WIDTH = 0
 
-		frame.CLASSBAR_WIDTH = 0;
-		frame.CLASSBAR_YOFFSET = 0;
-		frame.HAPPINESS_WIDTH = 0
-		frame.BOTTOM_OFFSET = 0;
+		frame.CLASSBAR_YOFFSET = 0
+		frame.BOTTOM_OFFSET = 0
 
-		frame.VARIABLES_SET = true;
+		frame.VARIABLES_SET = true
 	end
 
-	frame.colors = ElvUF.colors;
-	frame:RegisterForClicks(self.db.targetOnMouseDown and "AnyDown" or "AnyUp");
-
-	if(frame.isChild and frame.originalParent) then
-		local childDB = db.targetsGroup;
+	if frame.isChild and frame.originalParent then
+		local childDB = db.targetsGroup
 		frame.db = db.targetsGroup
-		if(not frame.originalParent.childList) then
-			frame.originalParent.childList = {};
+		if not frame.originalParent.childList then
+			frame.originalParent.childList = {}
 		end
-		frame.originalParent.childList[frame] = true;
+		frame.originalParent.childList[frame] = true
 
-		if(not InCombatLockdown()) then
-			if(childDB.enable) then
-				frame:SetParent(frame.originalParent);
-				frame:Size(childDB.width, childDB.height);
-				frame:ClearAllPoints();
-				frame:Point(E.InversePoints[childDB.anchorPoint], frame.originalParent, childDB.anchorPoint, childDB.xOffset, childDB.yOffset);
+		if not InCombatLockdown() then
+			if childDB.enable then
+				frame:SetParent(frame.originalParent)
+				frame:Size(childDB.width, childDB.height)
+				frame:ClearAllPoints()
+				frame:Point(E.InversePoints[childDB.anchorPoint], frame.originalParent, childDB.anchorPoint, childDB.xOffset, childDB.yOffset)
 			else
-				frame:SetParent(E.HiddenFrame);
+				frame:SetParent(E.HiddenFrame)
 			end
 		else
-			frame:SetAttribute("initial-height", childDB.height);
-			frame:SetAttribute("initial-width", childDB.width);
+			frame:SetAttribute("initial-height", childDB.height)
+			frame:SetAttribute("initial-width", childDB.width)
+		end
+	elseif not InCombatLockdown() then
+		frame:Size(frame.UNIT_WIDTH, frame.UNIT_HEIGHT)
+	end
+
+	--Health
+	UF:Configure_HealthBar(frame)
+
+	--Name
+	do
+		local name = frame.Name
+		name:Point("CENTER", frame.Health, "CENTER")
+		if UF.db.colors.healthclass then
+			frame:Tag(name, "[name:medium]")
+		else
+			frame:Tag(name, "[namecolor][name:medium]")
 		end
 	end
 
-	if(not InCombatLockdown()) then
-		frame.db = db;
-		frame:Size(db.width, db.height);
-	else
-		frame:SetAttribute("initial-height", frame.UNIT_HEIGHT);
-		frame:SetAttribute("initial-width", frame.UNIT_WIDTH);
+	--Threat
+	UF:Configure_Threat(frame)
+
+	--Range
+	UF:Configure_Range(frame)
+
+	if not frame.isChild then
+		--Auras
+		UF:EnableDisable_Auras(frame)
+		UF:Configure_Auras(frame, "Buffs")
+		UF:Configure_Auras(frame, "Debuffs")
+
+		--RaidDebuffs
+		UF:Configure_RaidDebuffs(frame)
+
+		--Debuff Highlight
+		UF:Configure_DebuffHighlight(frame)
+
+		--Buff Indicator
+		UF:UpdateAuraWatch(frame)
 	end
 
-	UF:Configure_HealthBar(frame);
-
-	local name = frame.Name;
-	name:Point("CENTER", frame.Health, "CENTER");
-	if(UF.db.colors.healthclass) then
-		frame:Tag(name, "[name:medium]");
-	else
-		frame:Tag(name, "[namecolor][name:medium]");
-	end
-
-	UF:Configure_Threat(frame);
-
-	UF:Configure_Range(frame);
-
-	if(not frame.isChild) then
-		UF:EnableDisable_Auras(frame);
-		UF:Configure_Auras(frame, "Buffs");
-		UF:Configure_Auras(frame, "Debuffs");
-
-		UF:Configure_RaidDebuffs(frame);
-
-		UF:Configure_DebuffHighlight(frame);
-
-		UF:UpdateAuraWatch(frame);
-	end
-
-	frame:UpdateAllElements("ElvUI_UpdateAllElements");
+	frame:UpdateAllElements("ElvUI_UpdateAllElements")
 end
 
-UF["headerstoload"]["tank"] = {"MAINTANK", "ELVUI_UNITTARGET"};
+UF["headerstoload"]["tank"] = {"MAINTANK", "ELVUI_UNITTARGET"}
