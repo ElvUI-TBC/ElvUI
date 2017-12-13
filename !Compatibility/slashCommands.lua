467e734805ba295b0f757e999f42328b7205b8c2
Pinya <800pin.ru@gmail.com>
2017-12-14 00:39:49 +0300

    add auto-enabling of !DebugTools when executing /fstack /dump /eventtrace commands

--- !Compatibility/slashCommands.lua
+++ !Compatibility/slashCommands.lua
@@ -1,30 +1,59 @@
+--Cache global variables
 local strmatch = strmatch
-local UIParentLoadAddOn = UIParentLoadAddOn
+--WoW API
+local IsAddOnLoaded = IsAddOnLoaded
+
+local checked
+local function LoadDebugTools()
+	if checked then return end
+
+	local _, _, _, _, _, reason = GetAddOnInfo("!DebugTools")
+	checked = true
+
+	if reason == "MISSING" then return end
+
+	if IsAddOnLoaded("!DebugTools") then
+		LoadAddOn("!DebugTools")
+	else
+		EnableAddOn("!DebugTools")
+		LoadAddOn("!DebugTools")
+		DisableAddOn("!DebugTools")
+	end
+end
 
 SLASH_FRAMESTACK1 = "/framestack"
 SLASH_FRAMESTACK2 = "/fstack"
 SlashCmdList["FRAMESTACK"] = function(msg)
-	UIParentLoadAddOn("!DebugTools")
-	local showHiddenArg, showRegionsArg = strmatch(msg, "^%s*(%S+)%s+(%S+)%s*$")
-	if (not showHiddenArg or not showRegionsArg) then
-		showHiddenArg = strmatch(msg, "^%s*(%S+)%s*$")
-		showRegionsArg = "1"
-	end
-	local showHidden = showHiddenArg == "true" or showHiddenArg == "1"
-	local showRegions = showRegions == "true" or showRegionsArg == "1"
+	LoadDebugTools()
 
-	FrameStackTooltip_Toggle(showHidden, showRegions)
+	if IsAddOnLoaded("!DebugTools") then
+		local showHiddenArg, showRegionsArg = strmatch(msg, "^%s*(%S+)%s+(%S+)%s*$")
+		if (not showHiddenArg or not showRegionsArg) then
+			showHiddenArg = strmatch(msg, "^%s*(%S+)%s*$")
+			showRegionsArg = "1"
+		end
+		local showHidden = showHiddenArg == "true" or showHiddenArg == "1"
+		local showRegions = showRegions == "true" or showRegionsArg == "1"
+
+		FrameStackTooltip_Toggle(showHidden, showRegions)
+	end
 end
 
 SLASH_EVENTTRACE1 = "/eventtrace"
 SLASH_EVENTTRACE2 = "/etrace"
 SlashCmdList["EVENTTRACE"] = function(msg)
-	UIParentLoadAddOn("!DebugTools")
-	EventTraceFrame_HandleSlashCmd(msg)
+	LoadDebugTools()
+
+	if IsAddOnLoaded("!DebugTools") then
+		EventTraceFrame_HandleSlashCmd(msg)
+	end
 end
 
 SLASH_DUMP1 = "/dump"
 SlashCmdList["DUMP"] = function(msg)
-	UIParentLoadAddOn("!DebugTools")
-	DevTools_DumpCommand(msg)
+	LoadDebugTools()
+
+	if IsAddOnLoaded("!DebugTools") then
+		DevTools_DumpCommand(msg)
+	end
 end
