6c9d4b2473892c8340661280f713dc861a5062df
Pinya <800pin.ru@gmail.com>
2017-12-14 00:09:09 +0300

    load developer hooks only in case !DebugTools enabled and loaded

--- ElvUI/developer/frame.lua
+++ ElvUI/developer/frame.lua
@@ -56,68 +56,70 @@ SlashCmdList["FRAME"] = function(arg)
 	end
 end
 
-CreateFrame("Frame", "FrameStackHighlight");
-FrameStackHighlight:SetFrameStrata("TOOLTIP");
-local t = FrameStackHighlight:CreateTexture(nil, "BORDER");
-t:SetAllPoints();
-t:SetTexture(0, 1, 0, 0.5);
+if IsAddOnLoaded("!DebugTools") then
+	CreateFrame("Frame", "FrameStackHighlight");
+	FrameStackHighlight:SetFrameStrata("TOOLTIP");
+	local t = FrameStackHighlight:CreateTexture(nil, "BORDER");
+	t:SetAllPoints();
+	t:SetTexture(0, 1, 0, 0.5);
 
-hooksecurefunc("FrameStackTooltip_Toggle", function()
-	local tooltip = _G["FrameStackTooltip"];
-	if(not tooltip:IsVisible()) then
-		FrameStackHighlight:Hide();
-	end
-end);
-
-local _timeSinceLast = 0
-FrameStackTooltip:HookScript("OnUpdate", function(self, elapsed)
-	_timeSinceLast = _timeSinceLast - elapsed;
-	if (_timeSinceLast <= 0) then
-		_timeSinceLast = FRAMESTACK_UPDATE_TIME;
-		local highlightFrame = UpdateFrameStack(self, self.showHidden)
-
-		FrameStackHighlight:ClearAllPoints();
-		if (highlightFrame and highlightFrame ~= _G["WorldFrame"]) then
-			FrameStackHighlight:SetPoint("BOTTOMLEFT", highlightFrame);
-			FrameStackHighlight:SetPoint("TOPRIGHT", highlightFrame);
-			FrameStackHighlight:Show();
-		else
+	hooksecurefunc("FrameStackTooltip_Toggle", function()
+		local tooltip = _G["FrameStackTooltip"];
+		if(not tooltip:IsVisible()) then
 			FrameStackHighlight:Hide();
 		end
-	end
-end);
+	end);
 
-SLASH_FRAMELIST1 = "/framelist"
-SlashCmdList["FRAMELIST"] = function(msg)
-	if(not FrameStackTooltip) then
-		UIParentLoadAddOn("!DebugTools");
-	end
+	local _timeSinceLast = 0
+	FrameStackTooltip:HookScript("OnUpdate", function(self, elapsed)
+		_timeSinceLast = _timeSinceLast - elapsed;
+		if (_timeSinceLast <= 0) then
+			_timeSinceLast = FRAMESTACK_UPDATE_TIME;
+			local highlightFrame = UpdateFrameStack(self, self.showHidden)
 
-	local isPreviouslyShown = FrameStackTooltip:IsShown()
-	if(not isPreviouslyShown) then
-		if(msg == tostring(true)) then
-			FrameStackTooltip_Toggle(true);
-		else
+			FrameStackHighlight:ClearAllPoints();
+			if (highlightFrame and highlightFrame ~= _G["WorldFrame"]) then
+				FrameStackHighlight:SetPoint("BOTTOMLEFT", highlightFrame);
+				FrameStackHighlight:SetPoint("TOPRIGHT", highlightFrame);
+				FrameStackHighlight:Show();
+			else
+				FrameStackHighlight:Hide();
+			end
+		end
+	end);
+
+	SLASH_FRAMELIST1 = "/framelist"
+	SlashCmdList["FRAMELIST"] = function(msg)
+		if(not FrameStackTooltip) then
+			UIParentLoadAddOn("!DebugTools");
+		end
+
+		local isPreviouslyShown = FrameStackTooltip:IsShown()
+		if(not isPreviouslyShown) then
+			if(msg == tostring(true)) then
+				FrameStackTooltip_Toggle(true);
+			else
+				FrameStackTooltip_Toggle();
+			end
+		end
+
+		print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+		for i = 2, FrameStackTooltip:NumLines() do
+			local text = _G["FrameStackTooltipTextLeft"..i]:GetText();
+			if(text and text ~= "") then
+				print(text)
+			end
+		end
+		print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+
+		if(CopyChatFrame:IsShown()) then
+			CopyChatFrame:Hide()
+		end
+
+		ElvUI[1]:GetModule("Chat"):CopyChat(ChatFrame1)
+		if(not isPreviouslyShown) then
 			FrameStackTooltip_Toggle();
 		end
-	end
-
-	print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
-	for i = 2, FrameStackTooltip:NumLines() do
-		local text = _G["FrameStackTooltipTextLeft"..i]:GetText();
-		if(text and text ~= "") then
-			print(text)
-		end
-	end
-	print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
-
-	if(CopyChatFrame:IsShown()) then
-		CopyChatFrame:Hide()
-	end
-
-	ElvUI[1]:GetModule("Chat"):CopyChat(ChatFrame1)
-	if(not isPreviouslyShown) then
-		FrameStackTooltip_Toggle();
 	end
 end
 
