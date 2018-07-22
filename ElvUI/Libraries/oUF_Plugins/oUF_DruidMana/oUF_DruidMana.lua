0582e8fdcfbdc9f88b0be59043142f64c2acdee1
Apollyonn <jellefenxd@gmail.com>
2018-07-23 02:52:08 +0300

    revert druid mana

--- ElvUI/Libraries/oUF_Plugins/oUF_DruidMana/oUF_DruidMana.lua
+++ ElvUI/Libraries/oUF_Plugins/oUF_DruidMana/oUF_DruidMana.lua
@@ -5,140 +5,114 @@ local oUF = ns.oUF
 
 local LDM = LibStub("LibDruidMana-1.0")
 
-local function UpdateColor(element, cur, max)
-	local parent = element.__owner
+local UnitIsPlayer = UnitIsPlayer
+local UnitPlayerControlled = UnitPlayerControlled
+local UnitClass = UnitClass
+local UnitReaction = UnitReaction
 
-	local r, g, b, t
-	if(element.colorClass) then
-		t = parent.colors.class['DRUID']
-	elseif(element.colorSmooth) then
-		r, g, b = parent.ColorGradient(cur, max, unpack(element.smoothGradient or parent.colors.smooth))
-	elseif(element.colorPower) then
-		t = parent.colors.power[0]
+local UPDATE_VISIBILITY = function(self, event)
+	local druidmana = self.DruidAltMana;
+
+	local min, max = druidmana:GetMinMaxValues();
+	local num, str = UnitPowerType("player");
+	if(num ~= 0) then
+		if(druidmana:GetValue() == max) then
+			druidmana:Hide();
+		else
+			druidmana:Show();
+		end
+	else
+		druidmana:Hide();
+	end
+
+	if(druidmana.PostUpdateVisibility) then
+		return druidmana:PostUpdateVisibility(self.unit);
+	end
+end
+
+local UNIT_MANA = function(self, event, unit, currMana, maxMana)
+	if(self.unit ~= unit) then return; end
+	local druidmana = self.DruidAltMana;
+
+	if(druidmana.PreUpdate) then
+		druidmana:PreUpdate(unit);
+	end
+
+	local min, max = currMana, maxMana
+
+	if not (min and max) then
+		min, max = LDM:GetCurrentMana(), LDM:GetMaximumMana()
+	end
+
+	druidmana:SetMinMaxValues(0, max);
+	druidmana:SetValue(min);
+
+	local r, g, b, t;
+	if(druidmana.colorPower) then
+		t = self.colors.power[0];
+	elseif(druidmana.colorClass and UnitIsPlayer(unit)) or
+		(druidmana.colorClassNPC and not UnitIsPlayer(unit)) or
+		(druidmana.colorClassPet and UnitPlayerControlled(unit) and not UnitIsPlayer(unit)) then
+		local _, class = UnitClass(unit);
+		t = self.colors.class[class];
+	elseif(druidmana.colorReaction and UnitReaction(unit, "player")) then
+		t = self.colors.reaction[UnitReaction(unit, "player")];
+	elseif(druidmana.colorSmooth) then
+		r, g, b = self.ColorGradient(min / max, unpack(druidmana.smoothGradient or self.colors.smooth));
 	end
 
 	if(t) then
-		r, g, b = t[1], t[2], t[3]
+		r, g, b = t[1], t[2], t[3];
 	end
 
 	if(b) then
-		element:SetStatusBarColor(r, g, b)
+		druidmana:SetStatusBarColor(r, g, b);
 
-		local bg = element.bg
+		local bg = druidmana.bg;
 		if(bg) then
-			local mu = bg.multiplier or 1
-			bg:SetVertexColor(r * mu, g * mu, b * mu)
+			local mu = bg.multiplier or 1;
+			bg:SetVertexColor(r * mu, g * mu, b * mu);
 		end
 	end
-end
 
-local function Update(self, event, unit, cur, max)
-	if(unit ~= 'player') then return end
+	UPDATE_VISIBILITY(self);
 
-	local element = self.DruidAltMana
-	if(element.PreUpdate) then element:PreUpdate(unit) end
-
-	if not (cur and max) then
-		cur, max = LDM:GetCurrentMana(), LDM:GetMaximumMana()
-	end
-
-	element:SetMinMaxValues(0, max)
-	element:SetValue(cur)
-
-	element:UpdateColor(cur, max)
-
-	if(element.PostUpdate) then
-		return element:PostUpdate(unit, cur, max, event)
+	if(druidmana.PostUpdate) then
+		return druidmana:PostUpdate(unit, min, max, event);
 	end
 end
 
-local function Path(self, ...)
-	return (self.DruidAltMana.Override or Update) (self, ...)
+local Update = function(self, ...)
+	UNIT_MANA(self, ...);
+	return UPDATE_VISIBILITY(self, ...);
 end
 
-local function ElementEnable(self)
-	self:RegisterEvent("UNIT_MANA", Path)
-	self:RegisterEvent("UNIT_MAXMANA", Path)
-
-	self.DruidAltMana:Show()
-
-	if self.DruidAltMana.PostUpdateVisibility then
-		self.DruidAltMana:PostUpdateVisibility(true, not self.DruidAltMana.isEnabled)
-	end
-
-	self.DruidAltMana.isEnabled = true
-
-	Path(self, 'ElementEnable', 'player', UnitPowerType("player", 0))
+local ForceUpdate = function(element)
+	return Update(element.__owner, "ForceUpdate");
 end
 
-local function ElementDisable(self)
-	self:UnregisterEvent("UNIT_MANA", Path)
-	self:UnregisterEvent("UNIT_MAXMANA", Path)
+local Enable = function(self, unit)
+	local druidmana = self.DruidAltMana;
+	if(druidmana and unit == "player") then
+		druidmana.__owner = self;
+		druidmana.ForceUpdate = ForceUpdate;
 
-	self.DruidAltMana:Hide()
-
-	if self.DruidAltMana.PostUpdateVisibility then
-		self.DruidAltMana:PostUpdateVisibility(false, self.DruidAltMana.isEnabled)
-	end
-
-	self.DruidAltMana.isEnabled = nil
-
-	Path(self, 'ElementDisable', 'player', UnitPowerType("player", 0))
-end
-
-local function Visibility(self, event)
-	local shouldEnable
-	local num = UnitPowerType("player")
-
-	if(num ~= 0) then
-		shouldEnable = true
-	end
-
-	if(shouldEnable) then
-		ElementEnable(self)
-	else
-		ElementDisable(self)
-	end
-end
-
-local function VisibilityPath(self, ...)
-	return (self.DruidAltMana.OverrideVisibility or Visibility) (self, ...)
-end
-
-local function ForceUpdate(element)
-	return VisibilityPath(element.__owner, 'ForceUpdate', element.__owner.unit)
-end
-
-local function Enable(self, unit)
-	local element = self.DruidAltMana
-	if(element and unit == 'player') then
-		element.__owner = self
-		element.ForceUpdate = ForceUpdate
-
-		LDM:AddListener(function(cur, max)
-			Update(self, "Listener", "player", cur, max)
+		LDM:AddListener(function(currMana, maxMana)
+			UNIT_MANA(self, "Listener", "player", currMana, maxMana)
 		end, "oUF_DruidMana")
-		self:RegisterEvent('UPDATE_SHAPESHIFT_FORM', VisibilityPath)
 
-		if(element:IsObjectType('StatusBar') and not element:GetStatusBarTexture()) then
-			element:SetStatusBarTexture([[Interface\TargetingFrame\UI-StatusBar]])
-		end
+		self:RegisterEvent("UPDATE_SHAPESHIFT_FORM", UPDATE_VISIBILITY);
 
-		if(not element.UpdateColor) then
-			element.UpdateColor = UpdateColor
-		end
-
-		return true
+		return true;
 	end
 end
 
-local function Disable(self)
-	local element = self.DruidAltMana
-	if(element) then
-		ElementDisable(self)
+local Disable = function(self)
+	local druidmana = self.DruidAltMana;
+	if(druidmana) then
 		LDM:RemoveListener("oUF_DruidMana")
 
-		self:UnregisterEvent('UPDATE_SHAPESHIFT_FORM', VisibilityPath)
+		self:UnregisterEvent("UPDATE_SHAPESHIFT_FORM", UPDATE_VISIBILITY);
 	end
 end
 
