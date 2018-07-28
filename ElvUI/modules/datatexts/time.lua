f210d21774f19ac5fad3c97d916de75ab1716487
Apollyonn <jellefenxd@gmail.com>
2018-07-28 14:47:55 +0300

    update

--- ElvUI/modules/datatexts/time.lua
+++ ElvUI/modules/datatexts/time.lua
@@ -1,83 +1,103 @@
-local E, L, V, P, G = unpack(ElvUI);
-local DT = E:GetModule("DataTexts");
+local E, L, V, P, G = unpack(ElvUI)
+local DT = E:GetModule("DataTexts")
 
-local time = time;
-local format, join = string.format, string.join;
+local unpack = unpack
+local format, join = string.format, string.join
+local time = time
 
-local GetGameTime = GetGameTime;
-local GetNumSavedInstances = GetNumSavedInstances;
-local GetSavedInstanceInfo = GetSavedInstanceInfo;
-local SecondsToTime = SecondsToTime;
+local GetGameTime = GetGameTime
+local GetNumSavedInstances = GetNumSavedInstances
+local GetSavedInstanceInfo = GetSavedInstanceInfo
+local SecondsToTime = SecondsToTime
+local TIMEMANAGER_TOOLTIP_REALMTIME = TIMEMANAGER_TOOLTIP_REALMTIME
 
-local timeDisplayFormat = "";
-local dateDisplayFormat = "";
-local europeDisplayFormat_nocolor = join("", "%02d", ":|r%02d");
-local lockoutInfoFormatNoEnc = "%s%s |cffaaaaaa(%s)";
-local difficultyInfo = {"N", "N", "H", "H"};
-local lockoutColorExtended, lockoutColorNormal = {r = 0.3, g = 1, b = 0.3}, {r = .8, g = .8, b = .8};
+local europeDisplayFormat = join("", "%02d", ":|r%02d")
+local instanceFormat = "%s |cffaaaaaa(%s)"
+local timeDisplayFormat = ""
+local dateDisplayFormat = ""
+local enteredFrame = false
+
+local lastPanel
 
 local function OnClick(_, btn)
-	if(btn == "RightButton") then
-		if(not IsAddOnLoaded("Blizzard_TimeManager")) then LoadAddOn("Blizzard_TimeManager"); end
-		TimeManagerClockButton_OnClick(TimeManagerClockButton);
+	if btn == "RightButton" then
+		if not IsAddOnLoaded("Blizzard_TimeManager") then LoadAddOn("Blizzard_TimeManager") end
+
+		TimeManagerClockButton_OnClick(TimeManagerClockButton)
 	else
-		GameTimeFrame:Click();
+		GameTimeFrame:Click()
 	end
 end
 
 local function OnLeave()
-	DT.tooltip:Hide();
+	DT.tooltip:Hide()
+
+	enteredFrame = false
 end
 
 local function OnEnter(self)
 	DT:SetupTooltip(self)
 	
-	RequestRaidInfo()
+	if not enteredFrame then
+		RequestRaidInfo()
 
-	local name, id, reset;
-	local oneraid, instanceid;
+		enteredFrame = true
+	end
+
+	local oneraid
+	local name, id, reset
 	for i = 1, GetNumSavedInstances() do
-		name, id, reset = GetSavedInstanceInfo(i);
-		if(name) then
-			if(not oneraid) then
-				DT.tooltip:AddLine(" ");
-				DT.tooltip:AddLine(L["Saved Raid(s)/Heroic Instance(s)"]);
-				oneraid = true;
+		name, id, reset = GetSavedInstanceInfo(i)
+		if name then
+			if not oneraid then
+				DT.tooltip:AddLine(L["Saved Instance(s)"])
+
+				oneraid = true
 			end
-			instanceid = format("%s (%d)", name, id)
-			DT.tooltip:AddDoubleLine(instanceid, SecondsToTime(reset, true), 1, 1, 1, lockoutColorNormal.r, lockoutColorNormal.g, lockoutColorNormal.b);
+
+			DT.tooltip:AddDoubleLine(format(instanceFormat, name, id), SecondsToTime(reset, true), 1, 1, 1, 0.8, 0.8, 0.8)
 		end
-		if i == 1 then
+
+		if DT.tooltip:NumLines() > 0 then
 			DT.tooltip:AddLine(" ")
 		end
 	end
 
-	DT.tooltip:AddDoubleLine(TIMEMANAGER_TOOLTIP_REALMTIME, format(europeDisplayFormat_nocolor, GetGameTime()), 1, 1, 1, lockoutColorNormal.r, lockoutColorNormal.g, lockoutColorNormal.b);
+	DT.tooltip:AddDoubleLine(TIMEMANAGER_TOOLTIP_REALMTIME, format(europeDisplayFormat, GetGameTime()), 1, 1, 1, 0.8, 0.8, 0.8)
 
-	DT.tooltip:Show();
+	DT.tooltip:Show()
 end
 
-local lastPanel;
-local int = 5;
+local function OnEvent(self, event)
+	if event == "UPDATE_INSTANCE_INFO" and enteredFrame then
+		OnEnter(self)
+	end
+end
+
+local int = 5
 local function OnUpdate(self, t)
-	int = int - t;
+	int = int - t
 
-	if(int > 0) then return; end
+	if int > 0 then return end
 
-	self.text:SetText(BetterDate(E.db.datatexts.timeFormat .. " " .. E.db.datatexts.dateFormat, time()):gsub(":", timeDisplayFormat):gsub("%s", dateDisplayFormat));
+	if enteredFrame then
+		OnEnter(self)
+	end
 
-	lastPanel = self;
-	int = 1;
+	self.text:SetText(BetterDate(E.db.datatexts.timeFormat.." "..E.db.datatexts.dateFormat, time()):gsub(":", timeDisplayFormat):gsub("%s", dateDisplayFormat))
+
+	lastPanel = self
+	int = 1
 end
 
 local function ValueColorUpdate(hex)
-	timeDisplayFormat = join("", hex, ":|r");
-	dateDisplayFormat = join("", hex, " ");
+	timeDisplayFormat = join("", hex, ":|r")
+	dateDisplayFormat = join("", hex, " ")
 
-	if(lastPanel ~= nil) then
-		OnUpdate(lastPanel, 20000);
+	if lastPanel ~= nil then
+		OnUpdate(lastPanel, 20000)
 	end
 end
-E["valueColorUpdateFuncs"][ValueColorUpdate] = true;
+E["valueColorUpdateFuncs"][ValueColorUpdate] = true
 
-DT:RegisterDatatext("Time", nil, nil, OnUpdate, OnClick, OnEnter, OnLeave);
+DT:RegisterDatatext("Time", {"UPDATE_INSTANCE_INFO"}, OnEvent, OnUpdate, OnClick, OnEnter, OnLeave)
