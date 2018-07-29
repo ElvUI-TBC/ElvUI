2b34f4c4624d29539fa71944803bff7c2e392c45
Apollyonn <jellefenxd@gmail.com>
2018-07-29 05:24:46 +0300

    Fix #377

--- ElvUI/modules/datatexts/spellpower.lua
+++ ElvUI/modules/datatexts/spellpower.lua
@@ -1,33 +1,40 @@
-local E, L, V, P, G = unpack(ElvUI);
-local DT = E:GetModule("DataTexts");
+local E, L, V, P, G = unpack(ElvUI)
+local DT = E:GetModule("DataTexts")
 
-local join = string.join;
+local join = string.join
 
-local GetSpellBonusDamage = GetSpellBonusDamage;
-local GetSpellBonusHealing = GetSpellBonusHealing;
+local GetSpellBonusDamage = GetSpellBonusDamage
+local GetSpellBonusHealing = GetSpellBonusHealing
 
-local displayNumberString = "";
-local lastPanel;
+local displayNumberString = ""
+local lastPanel
 
 local function OnEvent(self)
-	local spellDamage = GetSpellBonusDamage(7);
-	local spellHealing = GetSpellBonusHealing();
-
-	if(spellHealing > spellDamage) then
-		self.text:SetFormattedText(displayNumberString, L["HP"], spellHealing);
-	else
-		self.text:SetFormattedText(displayNumberString, L["SP"], spellDamage);
+	local holySchool = 2
+	local minModifier = GetSpellBonusDamage(holySchool)
+	local bonusHealing = GetSpellBonusHealing()
+	local bonusDamage
+	for i = (holySchool + 1), MAX_SPELL_SCHOOLS do
+		bonusDamage = GetSpellBonusDamage(i)
+		minModifier = max(minModifier, bonusDamage)
 	end
-	lastPanel = self;
+
+	if bonusHealing > minModifier then
+		self.text:SetFormattedText(displayNumberString, L["HP"], bonusHealing)
+	else
+		self.text:SetFormattedText(displayNumberString, L["SP"], minModifier)
+	end
+
+	lastPanel = self
 end
 
 local function ValueColorUpdate(hex)
-	displayNumberString = join("", "%s: ", hex, "%d|r");
+	displayNumberString = join("", "%s: ", hex, "%d|r")
 
-	if(lastPanel ~= nil) then
-		OnEvent(lastPanel);
+	if lastPanel ~= nil then
+		OnEvent(lastPanel)
 	end
 end
-E["valueColorUpdateFuncs"][ValueColorUpdate] = true;
+E["valueColorUpdateFuncs"][ValueColorUpdate] = true
 
 DT:RegisterDatatext("Spell/Heal Power", {"PLAYER_DAMAGE_DONE_MODS"}, OnEvent, nil, nil, nil, nil, L["Spell/Heal Power"])
