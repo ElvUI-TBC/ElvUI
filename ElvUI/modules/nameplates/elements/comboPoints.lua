73f4049044341a5a568f359807e930d6c61888ac
Apollyonn <jellefenxd@gmail.com>
2018-10-12 20:55:59 +0300

    update

--- ElvUI/modules/nameplates/elements/comboPoints.lua
+++ ElvUI/modules/nameplates/elements/comboPoints.lua
@@ -36,16 +36,16 @@ function mod:ConfigureElement_CPoints(frame)
 	if frame.UnitType == "FRIENDLY_PLAYER" or frame.UnitType == "FRIENDLY_NPC" then return end
 
 	local comboPoints = frame.CPoints
+	local healthShown = self.db.units[frame.UnitType].healthbar.enable or (frame.isTarget and self.db.alwaysShowTargetHealth)
 
 	comboPoints:ClearAllPoints()
-	if self.db.units[frame.UnitType].healthbar.enable or (frame.isTarget and self.db.alwaysShowTargetHealth) then
-		comboPoints:SetParent(frame.HealthBar)
+	if healthShown then
 		comboPoints:Point("CENTER", frame.HealthBar, "BOTTOM", self.db.units[frame.UnitType].comboPoints.xOffset, self.db.units[frame.UnitType].comboPoints.yOffset)
 	else
-		comboPoints:SetParent(frame)
 		comboPoints:Point("CENTER", frame, "TOP", self.db.units[frame.UnitType].comboPoints.xOffset, self.db.units[frame.UnitType].comboPoints.yOffset)
 	end
 
+	local width, height
 	for i = 1, MAX_COMBO_POINTS do
 		comboPoints[i]:SetStatusBarTexture(LSM:Fetch("statusbar", self.db.statusbar))
 		comboPoints[i]:SetStatusBarColor(unpack(E:GetColorTable(self.db.comboBar.colors[i])))
@@ -58,20 +58,23 @@ function mod:ConfigureElement_CPoints(frame)
 			comboPoints[i]:Point("LEFT", comboPoints[i - 1], "RIGHT", self.db.units[frame.UnitType].comboPoints.spacing, 0)
 		end
 
-		comboPoints[i]:Width(self.db.units[frame.UnitType].comboPoints.width)
-		comboPoints[i]:Height(self.db.units[frame.UnitType].comboPoints.height)
+		width = self.db.units[frame.UnitType].comboPoints.width
+		height = self.db.units[frame.UnitType].comboPoints.height
+
+		comboPoints[i]:Width(healthShown and width * (frame.ThreatScale or 1) * (self.db.useTargetScale and self.db.targetScale or 1) or width)
+		comboPoints[i]:Height(healthShown and height * (frame.ThreatScale or 1) * (self.db.useTargetScale and self.db.targetScale or 1) or height)
 	end
 end
 
 function mod:ConstructElement_CPoints(parent)
-	local comboBar = CreateFrame("Frame", "$parentComboPoints", parent.HealthBar)
+	local comboBar = CreateFrame("Frame", "$parentComboPoints", parent)
 	comboBar:SetSize(68, 1)
 	comboBar:Hide()
 
 	for i = 1, MAX_COMBO_POINTS do
 		comboBar[i] = CreateFrame("StatusBar", comboBar:GetName().."ComboPoint"..i, comboBar)
 
-		self:StyleFrame(comboBar[i], true)
+		self:StyleFrame(comboBar[i])
 	end
 
 	return comboBar
