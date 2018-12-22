80e17cbb61d119c40732d16b9e1f51f39858d471
Apollyonn <jellefenxd@gmail.com>
2018-12-22 09:57:59 +0200

    Update auras.lua

--- ElvUI/modules/nameplates/elements/auras.lua
+++ ElvUI/modules/nameplates/elements/auras.lua
@@ -332,11 +332,17 @@ end
 
 function mod:Auras_SizeChanged(width)
 	local numAuras = #self.icons
+	if numAuras == 0 then return end
+
+	local overrideWidth = self.db.widthOverride and self.db.widthOverride > 0 and self.db.widthOverride
+	local auraWidth = overrideWidth or (((width - numAuras) / numAuras) - (E.private.general.pixelPerfect and 0 or 3))
+	local auraHeight = (self.db.baseHeight or 18) * (self:GetParent().HealthBar.currentScale or 1)
+
 	for i = 1, numAuras do
-		self.icons[i]:SetWidth(self.db.widthOverride > 0 and self.db.widthOverride or ((width - numAuras) / numAuras) - (E.private.general.pixelPerfect and 0 or 3))
-		self.icons[i]:SetHeight((self.db.baseHeight or 18) * (self:GetParent().HealthBar.currentScale or 1))
+		self.icons[i]:SetWidth(auraWidth)
+		self.icons[i]:SetHeight(auraHeight)
 	end
-	self:SetHeight((self.db.baseHeight or 18) * (self:GetParent().HealthBar.currentScale or 1))
+	self:SetHeight(auraHeight)
 end
 
 function mod:UpdateAuraIcons(auras)
@@ -354,15 +360,18 @@ function mod:UpdateAuraIcons(auras)
 		self.Auras_SizeChanged(auras, auras:GetWidth(), auras:GetHeight())
 	end
 
+	local stackFont = LSM:Fetch("font", self.db.stackFont)
+	local aurasHeight = auras.db.baseHeight or 18
+
 	for i = 1, maxAuras do
 		auras.icons[i] = auras.icons[i] or tremove(auraCache) or mod:CreateAuraIcon(auras)
 		auras.icons[i]:SetParent(auras)
 		auras.icons[i]:ClearAllPoints()
 		auras.icons[i]:Hide()
-		auras.icons[i]:SetHeight(auras.db.baseHeight or 18)
+		auras.icons[i]:SetHeight(aurasHeight)
 
 		if auras.icons[i].count then
-			auras.icons[i].count:SetFont(LSM:Fetch("font", self.db.stackFont), self.db.stackFontSize, self.db.stackFontOutline)
+			auras.icons[i].count:SetFont(stackFont, self.db.stackFontSize, self.db.stackFontOutline)
 		end
 
 		if auras.icons[i].time then
